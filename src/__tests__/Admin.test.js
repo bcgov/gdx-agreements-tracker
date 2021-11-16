fb38926f54008d424a8aa3a82137aab42a64f7e7
Shawn Turple <shawn.turple@gov.bc.ca>
2021-11-16 15:36:31 -0800

    WD-3598 Adding routing, basic styling, and sub navs for Project and contract lists

---
+++ src/__tests__/Admin.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Routes } from "react-router-dom";
+import AdminRoutes from '../Admin/routes';
+
+test('renders Admin page', () => {
+    render(
+    <MemoryRouter initialEntries={['/admin']}>
+      <Routes key="main">{AdminRoutes}</Routes>
+    </MemoryRouter>
+    );
+    const linkElement = screen.getByText(/Admin/i);
+    expect(linkElement).toBeInTheDocument();
+});
