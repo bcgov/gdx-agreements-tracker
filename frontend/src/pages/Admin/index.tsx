db1895fc31109fb57241816719daef9d47b448aa
Shawn Turple <shawn.turple@gov.bc.ca>
2021-12-20 08:18:03 -0800

    WD-3737 WD-3736 FE user routes to api, added Keycloak Debug component, updated pull request template.

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,45 +1,60 @@
-import React, { FC, useEffect } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import React, { FC, useState, useEffect } from "react";
+import { Outlet, useParams, useOutletContext } from "react-router-dom";
 import apiAxios from "../../utils/apiAxios";
 import { useKeycloak } from "@react-keycloak/web";
 import Sidebar from "../../components/Sidebar";
 import Main from "../../components/Main";
+import Debug from "../../components/Debug";
+import type { KeycloakInstance } from "keycloak-js";
+
+interface OutletContext {
+  initialized: boolean;
+  keycloak: KeycloakInstance;
+}
 
 export const Admin: FC = () => {
   const { initialized, keycloak } = useKeycloak();
-
-  /**
-   * Check keycloak init and auth on component load.
-   *
-   * @todo Need a better way to get access to the keycloak token.
-   */
-  useEffect(() => {
-    if (initialized && keycloak.authenticated) {
-      const axiosResponse = apiAxios(keycloak.token);
-      axiosResponse
-        .get("user")
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  });
-
   return (
     <>
       <Sidebar />
       <Main>
-        <Outlet />
+        <Outlet context={{ initialized, keycloak }} />
+        <Debug keycloak={keycloak} />
       </Main>
     </>
   );
 };
 
 export const User: FC = () => {
+  const { initialized, keycloak } = useOutletContext<OutletContext>();
+  const [apiData, setApiData] = useState({});
   let { userId } = useParams();
-  return <h2>User {userId}</h2>;
+  useEffect(() => {
+    if (initialized && keycloak.authenticated) {
+      const axiosResponse = apiAxios(keycloak.token);
+      let uri = "user";
+      if (userId) {
+        uri = `${uri}/${userId}`;
+      }
+      axiosResponse
+        .get(uri)
+        .then((data) => {
+          setApiData(data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [initialized, keycloak, userId]);
+
+  return (
+    <div>
+      <h2>User {userId}</h2>
+      <div>
+        <pre>{JSON.stringify(apiData, null, 2)}</pre>
+      </div>
+    </div>
+  );
 };
 
 export const Contacts: FC = () => {
