723728bc0eaeb9853ec3c0883186c6a92a4d5f02
Craig Robertson <craig.robertson@gov.bc.ca>
2022-01-10 19:22:19 -0700

    openshift improvements and structure cleanup.

---
+++ backend/src/helpers/secrets.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const loadSecretFileOrUseEnv = (filePath, envName) => {
+  try {
+    return fs.readFileSync(filePath, 'utf8');
+  } catch (err) {
+    return process.env[envName];
+  }
+}
+
+module.exports = { loadSecretFileOrUseEnv }
