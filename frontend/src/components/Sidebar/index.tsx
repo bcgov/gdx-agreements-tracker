23ec5c056d2cea41d654a89b50d1e8cb9c86f4a1
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-01-28 13:23:48 -0800

    fixed linter errors

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 import { Link } from "react-router-dom";
 /**
- * @todo Need to make drawerWidth more dynamic 
+ * @todo Need to make drawerWidth more dynamic
  */
 //Used for calculcation for body width
 const drawerWidth = 400;
@@ -25,14 +25,14 @@ const drawerWidth = 400;
 /** The sidebar is the navigation menu located on the left side of the App */
 export const Sidebar = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
-/**used to toggle mobile and desktop views for the menu */
+  /**used to toggle mobile and desktop views for the menu */
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-/**
- * @todo Need to make adminLinks more dynamic (maybe a database?)
- * 
- */
+  /**
+   * @todo Need to make adminLinks more dynamic (maybe a database?)
+   *
+   */
   const adminLinks = [
     {
       name: "Contracts",
@@ -63,10 +63,10 @@ export const Sidebar = () => {
       link: "admin/glossary",
     },
   ];
-/**
- * @todo Need to add an accordian to the menu to allow for expandable menus
- * 
- */
+  /**
+   * @todo Need to add an accordian to the menu to allow for expandable menus
+   *
+   */
 
   /**const drawer = The actual links in the sidebar navigation */
 
