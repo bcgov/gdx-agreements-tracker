58ef1519928ebd3d919a16057b29b20b513c1dd7
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-01-28 13:22:11 -0800

    added comments

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,30 +1,3 @@
-// import React, { FC } from "react";
-// import ButtonNav from "../ButtonNav";
-
-// interface Props {
-//   state?: Object;
-// }
-
-// export const Sidebar: FC<Props> = ({ state }) => {
-//   return (
-//     <aside>
-//       <section data-testId={"sideBarSection"} className="nav-section">
-//         <h2>GDX Agreements Tracker</h2>
-//         <nav>
-//           <ButtonNav to={`/project`}>Project List</ButtonNav>
-//           <ButtonNav to={`/project/34`}>Project Item Sample</ButtonNav>
-//           <ButtonNav to={`/contract`}>Contract List</ButtonNav>
-//           <ButtonNav to={`/contract/34`}>Contract Item Sample</ButtonNav>
-//           <ButtonNav to={`/reports`}>Reports</ButtonNav>
-//           <ButtonNav to={`/admin`}>Administration Forms</ButtonNav>
-//         </nav>
-//       </section>
-//       <img src="/gov_bc_logo.svg" alt="BC government logo" />
-//     </aside>
-//   );
-// };
-
-// export default Sidebar;
 import * as React from "react";
 
 import "./sideBar.scss";
@@ -43,15 +16,23 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 
 import { Link } from "react-router-dom";
-
+/**
+ * @todo Need to make drawerWidth more dynamic 
+ */
+//Used for calculcation for body width
 const drawerWidth = 400;
+
+/** The sidebar is the navigation menu located on the left side of the App */
 export const Sidebar = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
-
+/**used to toggle mobile and desktop views for the menu */
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
+/**
+ * @todo Need to make adminLinks more dynamic (maybe a database?)
+ * 
+ */
   const adminLinks = [
     {
       name: "Contracts",
@@ -82,6 +63,12 @@ export const Sidebar = () => {
       link: "admin/glossary",
     },
   ];
+/**
+ * @todo Need to add an accordian to the menu to allow for expandable menus
+ * 
+ */
+
+  /**const drawer = The actual links in the sidebar navigation */
 
   const drawer = (
     <div>
