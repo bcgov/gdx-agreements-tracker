4b859802b13406b3589f19fd014aff394232d0b3
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-03-24 13:39:40 -0700

    Added migrations and seeds for contacts table

---
+++ backend/src/database/seeds/04_contacts.js
@@ -0,0 +1,15 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('contacts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('contacts').insert([
+        {id: 1, first_name: 'Mike',last_name:'Lara',job_title:'Technical Analyst',ministry_id:'2',notes:'This is a note about Lara',},
+        {id: 2, first_name: 'Sarah',last_name:'Gonzalez',job_title:'Business Analyst',ministry_id:'3',notes:'This is a note about Sarah',},
+        {id: 3, first_name: 'John',last_name:'Lara',job_title:'Developer',ministry_id:'2',notes:'This is a note about John',},
+        {id: 4, first_name: 'Jacob',last_name:'Valencia',job_title:'IT Admin',ministry_id:'3',notes:'This is a note about Jacob',},
+        {id: 5, first_name: 'Eric',last_name:'Richardson',job_title:'Director',ministry_id:'2',notes:'This is a note about Eric',}
+      ]);
+    });
+};
