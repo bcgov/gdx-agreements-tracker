9500107b761e5084ceefa7f7099a80929e252255
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-03-24 13:50:14 -0700

    fixed linter errors

--- backend/src/database/migrations/20220324200600_create_contacts_table.js
+++ backend/src/database/migrations/20220324200600_create_contacts_table.js
@@ -1,17 +1,16 @@
-
-exports.up = function(knex) {
-    return knex.schema.createTable("contacts", function (table) {
-        table.increments();//id
-        table.string("first_name").notNullable();
-        table.string("last_name").notNullable();
-        table.string("job_title");
-        table.string("ministry_id");
-        table.text("notes");
-        table.timestamp("created_at").defaultTo(knex.fn.now());
-        table.timestamp("updated_at").defaultTo(knex.fn.now());
-      });
+exports.up = function (knex) {
+  return knex.schema.createTable("contacts", function (table) {
+    table.increments(); //id
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("job_title");
+    table.string("ministry_id");
+    table.text("notes");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable("contacts");
+exports.down = function (knex) {
+  return knex.schema.dropTable("contacts");
 };
