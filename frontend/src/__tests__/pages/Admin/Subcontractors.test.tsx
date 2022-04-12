8d3d81a625367b1cc1184bd02855866809b00c62
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-04-12 09:59:11 -0700

    Feature/descw 276 suppliers router (#58)
    * Added placeholder and updated route-subcontractors
    
    * Added test for subscontractors route
    
    * updated changelog
    
    * Added an assertion
    
    * fixed linter errors

---
+++ frontend/src/__tests__/pages/Admin/Subcontractors.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
+import { MemoryRouter, Routes } from "react-router-dom";
+import adminRoutes from "../../../routes/subRoutes/adminRoutes";
+import { Subcontractors } from "../../../pages/Admin/Subcontractors";
+
+//Mock keycloak.
+jest.mock("@react-keycloak/web", () => ({
+  useKeycloak: () => ({ initialized: true, keycloak: { authenticated: true } }),
+}));
+
+describe("<Subcontractors /> routing", () => {
+  it("renders Subcontractors page when '/admin/subcontractors' is hit", () => {
+    render(
+      <MemoryRouter initialEntries={["/admin/subcontractors"]}>
+        <Routes key="main">{adminRoutes}</Routes>
+      </MemoryRouter>
+    );
+    const wrapper = shallow(<Subcontractors />);
+    expect(wrapper.text().includes("Subcontractors")).toBe(true);
+  });
+});
---
+++ frontend/src/pages/Admin/Subcontractors/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const Subcontractors = () => {
+  return (
+    <div style={{ height: "50vh", width: "200%" }}>
+      <Typography variant="h5" component="h2">
+        Subcontractors
+      </Typography>
+      <h1>Placeholder (Table goes here)</h1>
+    </div>
+  );
+};
