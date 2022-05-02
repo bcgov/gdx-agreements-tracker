e1f5052d655e866ded229f02ef3be485983bdebc
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-05-02 09:09:01 -0700

    Basics for layout changes

--- frontend/src/bcgovTheme.ts
+++ frontend/src/bcgovTheme.ts
@@ -1,4 +1,4 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, SxProps } from "@mui/material/styles";
 
 /**
  * Create a theme that uses BC Gov colours.
@@ -6,14 +6,30 @@ import { createTheme } from "@mui/material/styles";
  * @see https://mui.com/customization/theming/
  */
 
+
 const bcgovTheme = createTheme({
   palette: {
     primary: {
       main: "#036",
+      contrastText: '#fff',
     },
     secondary: {
       main: "#38598a",
     },
+  },
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          background: "#38598a"
+        }
+      }
+    }
+  },
+  customSettings: {
+    BCGovAccentLine: "2px solid #fcba19",
+    topBarHeight: "50px",
+    drawerWidth: 240,
   },
 });
 
--- frontend/src/types/index.tsx
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  created_at: string;
-  email: string;
-  id: number;
-  name: string;
-  updated_at: string;
-  username: string;
-  roles: any[];
-}
