c0ae0e5974b5c9f52ac025947a9c43812d08e277
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-05-02 09:56:51 -0700

    Fixed structual pieces that compliment the components

--- frontend/src/pages/Admin/Subcontractors/index.tsx
+++ frontend/src/pages/Admin/Subcontractors/index.tsx
@@ -6,11 +6,11 @@ import { Table } from "../../../components";
 export const Subcontractors: FC = () => {
   const { rows, columns, loading } = useFormatTableData("subcontractors");
   return (
-    <div style={{ height: "50vh", width: "200%" }}>
+    <>
       <Typography variant="h5" component="h2">
         Subcontractors
       </Typography>
       <Table columns={columns} rows={rows} loading={loading} />
-    </div>
+    </>
   );
 };
