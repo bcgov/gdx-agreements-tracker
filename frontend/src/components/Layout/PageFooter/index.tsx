a4e998c73b4e390180a84a19eceaa8cb1cb92926
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-05-02 09:26:16 -0700

    Added header, footer, sidebar and page layout

---
+++ frontend/src/components/Layout/PageFooter/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import bcgovTheme from "../../../bcgovTheme";
+
+const StyledFooter = styled(Box)({
+  borderTop: bcgovTheme.customSettings.BCGovAccentLine,
+  padding: "8px 24px",
+  marginTop: "auto",
+});
+
+export const PageFooter = () => (
+  <StyledFooter component="footer" bgcolor="primary.main" color="primary.contrastText">
+    <Typography noWrap>Footer</Typography>
+  </StyledFooter>
+);
---
+++ frontend/src/components/Layout/PageHeader/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { Sidebar } from "../..";
+import { ThemeProvider } from "@mui/material/styles";
+import bcgovTheme from "../../../bcgovTheme";
+import { createTheme } from "@mui/material/styles";
+import { useDrawer } from "../../../hooks/useDrawer";
+import { IPageHeader } from "../../../types";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const drawerWidth = 240;
+
+const StyledAppBar = styled(AppBar)({
+  borderBottom: bcgovTheme.customSettings.BCGovAccentLine,
+});
+
+export const PageHeader = ({ drawerOpen, handleDrawerToggle }: IPageHeader) => {
+  return (
+      <StyledAppBar
+        position="sticky"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Title Bar
+          </Typography>
+        </Toolbar>
+      </StyledAppBar>
+  );
+};
