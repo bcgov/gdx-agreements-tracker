1354c51e536934ebbf1ca025dea098731eb20199
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-05-10 15:34:44 -0700

    Added project details
    Co-authored-by: CR-gbc <CR-gbc@users.noreply.github.com>

--- frontend/src/pages/Projects/Project/index.tsx
+++ frontend/src/pages/Projects/Project/index.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import { TextField } from "@mui/material";
+import React, { useLayoutEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { DetailsList } from "../../../components";
 import { Outlet } from "react-router-dom";
+import { apiAxios } from "../../../utils";
 
 export const Project = () => {
+  const [projectInfo, setProjectInfo] = useState({ id: "dummy" });
+
   const { projectId } = useParams();
+
+  useLayoutEffect(() => {
+    // if ("number" === typeof projectId) {
+    // }
+    apiAxios()
+      .get(`projects/${projectId}`)
+      .then((project) => {
+        setProjectInfo(project.data);
+        /* eslint "no-warning-comments": [1, { "terms": ["todo", "fixme"] }] */
+        // todo: Define a good type. "Any" type temporarily permitted.
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      })
+      .catch((error) => {
+        console.error(error);
+      }); //! TODO: We had to ignore react-hooks/exhaustive-deps because of error "React Hook useLayoutEffect has a missing dependency: 'tableName'. Either include it or remove the dependency array" ref: https://exerror.com/react-hook-useeffect-has-a-missing-dependency/
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
-      <DetailsList id={projectId} title={"project"} />
+      {Object.entries(projectInfo).map(([key, value]) => {
+        return (
+          <>
+            <br />
+            <TextField disabled label={key} defaultValue={value} />
+            <br />
+          </>
+        );
+      })}
       <Outlet />
     </>
   );
