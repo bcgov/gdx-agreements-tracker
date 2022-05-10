b263122c40802f00a79603af10afc9a16c976210
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-05-10 07:02:32 -0700

    added test (#86)

--- frontend/src/__tests__/pages/Project.test.js
+++ frontend/src/__tests__/pages/Project.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import projectRoutes from "../../routes/subRoutes/projectRoutes";
-import { MemoryRouter, Routes } from "react-router-dom";
+import { MemoryRouter, Routes, Router } from "react-router-dom";
 import { Projects } from "../../pages";
 import { shallow } from "enzyme";
+import { createMemoryHistory } from "history";
 
 //Mock keycloak.
 jest.mock("@react-keycloak/web", () => ({
@@ -18,7 +19,19 @@ describe("<Projects /> routing", () => {
         <Routes key="main">{projectRoutes}</Routes>
       </MemoryRouter>
     );
-    const wrapper = shallow(<Projects />);
-    expect(wrapper.exists()).toBe(true);
+    shallow(<Projects />);
+  });
+
+  it("should redirect and update history", () => {
+    const history = createMemoryHistory();
+
+    render(
+      <Router location={history.location} navigator={history}>
+        <Projects />
+      </Router>
+    );
+
+    fireEvent.click(screen.getByText(/New Project/i));
+    expect(history.location.pathname).toEqual("/projects/new");
   });
 });
