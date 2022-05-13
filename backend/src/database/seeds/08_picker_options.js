68146396240fe212b4dfde458e965d739ec9ae8c
kshapka-bcgov <102179447+kshapka-bcgov@users.noreply.github.com>
2022-05-13 14:48:51 -0600

    DESCW-350 DESCW-351 DESCW-352 DESCW-353 DESCW-354 agreement, project, funding and recovery details picklist seeds

--- backend/src/database/seeds/08_picker_options.js
+++ backend/src/database/seeds/08_picker_options.js
@@ -29,6 +29,94 @@ exports.seed = function (knex) {
             },
           },
         },
+        {
+          id: 2,
+          name: "agreement_type",
+          title: "Agreement Type",
+          description: "The type of agreement.",
+          definition: {
+            partnership_agreement: {
+              type: "string",
+              title: "Partnership Agreement",
+            },
+            mou: {
+              type: "string",
+              title: "MOU",
+            },
+            project_charter: {
+              type: "string",
+              title: "Project Charter",
+            },
+            other: {
+              type: "string",
+              title: "Other",
+            },
+          },
+        },
+        {
+          id: 3,
+          name: "project_type",
+          title: "Project Type",
+          description: "The type of project.",
+          definition: {
+            internal: {
+              type: "string",
+              title: "Internal",
+            },
+            external: {
+              type: "string",
+              title: "External",
+            },
+            social_media: {
+              type: "string",
+              title: "Social Media",
+            },
+            service: {
+              type: "string",
+              title: "Service",
+            },
+          },
+        },
+        {
+          id: 4,
+          name: "funding",
+          title: "Funding",
+          description: "The type of funding.",
+          definition: {
+            operational: {
+              type: "string",
+              title: "Operational",
+            },
+            capital: {
+              type: "string",
+              title: "Capital",
+            },
+            combination: {
+              type: "string",
+              title: "Combination",
+            },
+          },
+        },
+        {
+          id: 5,
+          name: "recovery_details",
+          title: "Recovery Details",
+          description: "The recovery details.",
+          definition: {
+            fully: {
+              type: "string",
+              title: "Fully",
+            },
+            partially: {
+              type: "string",
+              title: "Partially",
+            },
+            non_recoverable: {
+              type: "string",
+              title: "Non - Recoverable",
+            },
+          },
+        },
       ]);
     });
 };
