d28357497fc96109f304838577b12ba6a02006cd
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-06-02 11:50:09 -0700

    Update capabilities.js

--- backend/src/models/capabilities.js
+++ backend/src/models/capabilities.js
@@ -2,7 +2,7 @@ const DatabaseConnection = require("../database/databaseConnection");
 const dbConnection = new DatabaseConnection();
 const db = dbConnection.knex;
 
-const table = `${dbConnection.dataBaseSchemas()[2]}.capabilities`;
+const table = `${dbConnection.dataBaseSchemas().public}.capabilities`;
 
 // Get all.
 const findAll = () => {
