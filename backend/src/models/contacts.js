43c42dab4d43d3baf8f32def71bc1f73c869b880
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-06-02 11:50:21 -0700

    Update backend/src/models/contacts.js
    Co-authored-by: Shawn <45861534+ShawnTurple@users.noreply.github.com>

--- backend/src/models/contacts.js
+++ backend/src/models/contacts.js
@@ -2,7 +2,7 @@ const DatabaseConnection = require("../database/databaseConnection");
 const dbConnection = new DatabaseConnection();
 const db = dbConnection.knex;
 
-const table = `${dbConnection.dataBaseSchemas()[2]}.contacts`;
+const table = `${dbConnection.dataBaseSchemas().public}.contacts`;
 
 // Get all.
 const findAll = () => {
