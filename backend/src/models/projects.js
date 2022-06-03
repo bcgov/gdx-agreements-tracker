4bb960b3fed405d31b12cb9292bc92c71722e4a7
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-06-03 15:19:30 -0700

    added filter for project data

--- backend/src/models/projects.js
+++ backend/src/models/projects.js
@@ -5,7 +5,17 @@ const db = dbConnection.knex;
 const table = `${dbConnection.dataBaseSchemas().data}.project`;
 // Get all.
 const findAll = () => {
-  return db(table);
+  return db(table).select(
+    "project_number",
+    "project_name",
+    "project_version",
+    "portfolio_id",
+    "project_manager",
+    "agreement_end_date",
+    "project_status",
+    "initiation_date",
+    "id"
+  );
 };
 
 // Get specific one by id.
