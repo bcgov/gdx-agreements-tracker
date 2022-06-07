4bcb568b36d3ab2e9d07b77412a503d3d1498894
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-06-07 09:10:30 -0700

    added layout styling

--- frontend/src/components/ProjectLayout/index.tsx
+++ frontend/src/components/ProjectLayout/index.tsx
@@ -1,24 +1,35 @@
-import { Box, styled, Typography } from "@mui/material";
+import { Box, styled, Typography, useTheme } from "@mui/material";
 import React from "react";
 import { IProjectLayout } from "../../types";
 
-const StyledSectionTitle = styled(Typography)({
-  backgroundColor: "#f1f1f1",
-  textIndent: "10px",
-  margin:"-10px"
-});
-
-const StyledSectionContainer = styled(Box)({
-  border: "solid 3px #f1f1f1",
-  borderRadius: "4px",
-  padding: "10px",
-});
-
 export const ProjectLayout = ({ children }: IProjectLayout) => {
+  const theme = useTheme();
+
+  const StyledSectionTitle = styled(Typography)({
+    backgroundColor: "#f1f1f1",
+    textIndent: "10px",
+
+  });
+
+  const StyledSectionContainer = styled("div")({
+    border: "solid 3px #f1f1f1",
+    borderRadius: "4px",
+    padding: "10px",
+  });
+
+  const StyledBoxHolder = styled("div")({
+    [theme.breakpoints.down("md")]: {
+      columnCount: 1,
+    },
+    [theme.breakpoints.up("md")]: {
+      columnCount: 2,
+    },
+  });
+
   return (
     <StyledSectionContainer>
       <StyledSectionTitle variant="h6">Project Registration</StyledSectionTitle>
-      {children}
+      <StyledBoxHolder>{children}</StyledBoxHolder>
     </StyledSectionContainer>
   );
 };
