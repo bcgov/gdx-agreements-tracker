c80af1ae05a16cea36271bb0d34e02c1b0d8f47c
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-06-14 10:02:01 -0700

    Added Accordians

--- frontend/src/pages/Projects/Project/index.tsx
+++ frontend/src/pages/Projects/Project/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { GDX_Accordion } from "../../../components/GDX_Accordion";
 import { apiAxios } from "../../../utils";
 
 export const Project = () => {
@@ -21,15 +22,36 @@ export const Project = () => {
       {true === query.isLoading ? (
         <div>Loading</div>
       ) : (
-        Object.entries(query.data?.data).map(([key, value]) => {
-          return (
-            <div key={key}>
-              <br />
-              <TextField disabled label={key} defaultValue={value} />
-              <br />
-            </div>
-          );
-        })
+        <>
+          <GDX_Accordion sectionTitle="Project Registration">
+            {/* Added a slice just to show a few projects for now. */}
+            {Object.entries(query.data?.data).slice(1,4).map(([key, value]) => {
+              return (
+                <div key={key}>
+                  <br />
+                  <TextField disabled label={key} defaultValue={value} />
+                  <br />
+                </div>
+              );
+            })}
+          </GDX_Accordion>
+          <br/>
+          <GDX_Accordion sectionTitle="Contacts">
+          <h3>Contacts Placeholder</h3>
+          </GDX_Accordion>
+          <br/>
+          <GDX_Accordion sectionTitle="Deliverables">
+          <h3>Deliverables Placeholder</h3>
+          </GDX_Accordion>
+          <br/>
+          <GDX_Accordion sectionTitle="Client Coding">
+          <h3>Client Coding Placeholder</h3>
+          </GDX_Accordion>
+          <br/>
+          <GDX_Accordion sectionTitle="Budget">
+            <h3>Budget Placeholder</h3>
+          </GDX_Accordion>
+        </>
       )}
       <Outlet />
     </>
