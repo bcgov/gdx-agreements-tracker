8552cbadf67c2f4f9c4da5683b6052c09a1c4a81
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-06-14 15:40:22 -0700

    fixed tests

--- frontend/src/__tests__/pages/Users.test.js
+++ frontend/src/__tests__/pages/Users.test.js
@@ -4,7 +4,10 @@ import { render } from "@testing-library/react";
 import { MemoryRouter, Routes } from "react-router-dom";
 import adminRoutes from "../../routes/subRoutes/adminRoutes";
 import { Users } from "../../pages/";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+// Create a client
+const queryClient = new QueryClient();
 //Mock keycloak.
 jest.mock("@react-keycloak/web", () => ({
   useKeycloak: () => ({ initialized: true, keycloak: { authenticated: true } }),
@@ -13,11 +16,17 @@ jest.mock("@react-keycloak/web", () => ({
 describe("<Admin /> routing", () => {
   it("renders Admin page when '/admin' is hit", () => {
     render(
-      <MemoryRouter initialEntries={["/admin/users"]}>
-        <Routes key="main">{adminRoutes}</Routes>
-      </MemoryRouter>
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter initialEntries={["/admin/users"]}>
+          <Routes key="main">{adminRoutes}</Routes>
+        </MemoryRouter>
+      </QueryClientProvider>
     );
-    const wrapper = shallow(<Users />);
+    const wrapper = shallow(
+      <QueryClientProvider client={queryClient}>
+        <Users />
+      </QueryClientProvider>
+    );
     expect(wrapper.exists()).toBe(true);
   });
 });
