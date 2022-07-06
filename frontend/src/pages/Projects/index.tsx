7253a107fbecd73fb41a8be03208aac519a9df1b
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-07-06 09:24:56 -0700

    DESCW-417 fixed switch statement

--- frontend/src/pages/Projects/index.tsx
+++ frontend/src/pages/Projects/index.tsx
@@ -8,23 +8,25 @@ export const Projects: FC = () => {
     tableName: "projects",
     ApiEndPoint: "projects",
   });
+  
+  const switchRender = () => {
+    switch (isLoading) {
+      case true:
+        return <LinearProgress />;
+
+      case false:
+        return <Table columns={data.columns} rows={data.rows} loading={isLoading} />;
+      default:
+        return <LinearProgress />;
+    }
+  };
 
   return (
     <>
       <Typography variant="h5" component="h2">
         Projects
       </Typography>
-      {(() => {
-        switch (isLoading) {
-          case true:
-            return <LinearProgress />;
-
-          case false:
-            return <Table columns={data.columns} rows={data.rows} loading={isLoading} />;
-          default:
-            return <LinearProgress />;
-        }
-      })()}
+      {switchRender()}
       <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
         <Button component={Link} to={"/projects/new"} variant="contained">
           New Project
