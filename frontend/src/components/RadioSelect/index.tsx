e941e593702eea414328167d97a8ae9923faa136
kshapka-bcgov <102179447+kshapka-bcgov@users.noreply.github.com>
2022-08-15 23:37:13 -0600

    reusable radiogroups

--- frontend/src/components/RadioSelect/index.tsx
+++ frontend/src/components/RadioSelect/index.tsx
@@ -1,23 +1,27 @@
-import React, { ReactElement } from "react";
-import { DataGrid, GridEventListener, GridEvents } from "@mui/x-data-grid";
-import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, ListItemSecondaryAction, } from "@mui/material";
+import React, { ChangeEvent, ChangeEventHandler, ReactElement, useState } from "react";
+import { Box, Radio, styled, RadioGroup, FormControlLabel, FormControlLabelProps, FormControl, FormLabel, useRadioGroup, } from "@mui/material";
+import { EventType } from "@testing-library/react";
 
-export const RadioSelect = ({ formLabel, defaultValue, name, data }: { formLabel: string, defaultValue: string, name: string, data:{ value:string, label:string }[] }) => {
-  let radioButtons:Array<JSX.Element> = [];
-  data.forEach(radioButton => {
-    radioButtons.push(<FormControlLabel value={radioButton.value} control={<Radio />} label={radioButton.label} key={radioButton.value} />)
-  });
+export const RadioSelect = ({ formLabel, defaultValue, name, options }:
+  { formLabel: string, defaultValue: string, name: string, options:{parent:string, value:string, label:string}[]} ) => {
 
-    return (
+  const [selectedValue, setSelection] = useState(defaultValue)
+
+  function onChangeValue(event:React.FormEvent<HTMLInputElement>) {
+    setSelection(event.currentTarget.value);
+    //console.log(event.currentTarget.name, '-', event.currentTarget.value);
+  }
+
+  return (
     <FormControl>
-            <FormLabel id="radio-buttons-group">{formLabel}</FormLabel>
-            <RadioGroup
-                aria-labelledby="radio-buttons-group"
-                defaultValue={defaultValue}
-                name={name}
-            >
-                {radioButtons}
-            </RadioGroup>
-        </FormControl>
+      <FormLabel id="radio-control-group">{formLabel}</FormLabel>
+      <Box border={2} borderRadius={1} padding={1}>
+        <RadioGroup id={name} name={name} value={selectedValue} defaultValue={defaultValue} onChange={onChangeValue}>
+          {options.map((radioButton) => {
+            return <FormControlLabel value={radioButton.value} label={radioButton.label} key={radioButton.value} control={<Radio />} />
+          })}
+        </RadioGroup>
+      </Box>
+    </FormControl>
   );
 };
