56de338395514cebbeed82945072699e83abebe6
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-08-17 15:19:20 -0700

    suppliers example

---
+++ backend/src/models/project_status.js
@@ -0,0 +1,20 @@
+const DatabaseConnection = require("../database/databaseConnection");
+const dbConnection = new DatabaseConnection();
+const db = dbConnection.knex;
+
+const table = `${dbConnection.dataBaseSchemas().data}.project_status`;
+
+// Get all.
+const findAll = () => {
+  return db(table);
+};
+
+// Get specific one by id.
+const findById = (projectStatusId) => {
+  return db(getFromView).where("id", projectStatusId);
+};
+
+module.exports = {
+  findAll,
+  findById,
+};
