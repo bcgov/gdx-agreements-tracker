e69eeafa2bef057071223c32bef68c39ea2cb99b
kshapka-bcgov <102179447+kshapka-bcgov@users.noreply.github.com>
2022-08-17 22:04:56 -0600

    add the rest of the parameters and proper types

--- frontend/src/routes/subRoutes/reportRoutes.tsx
+++ frontend/src/routes/subRoutes/reportRoutes.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Reports, Report } from "../../pages";
-const reportRoutes = [
-  <Route key="report" path="reports" element={<Reports />} />,
-];
+import { Reports } from "../../pages";
+const reportRoutes = [<Route key="report" path="reports" element={<Reports />} />];
 
 export default reportRoutes;
