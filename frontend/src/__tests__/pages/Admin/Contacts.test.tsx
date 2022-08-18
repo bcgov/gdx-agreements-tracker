8e9bd6e6c4f325337099f1bb2e8947856e51820f
mhaswell <michael.haswell@gov.bc.ca>
2022-08-18 16:39:56 -0700

    Updated changelog. Fixed Contacts test async issue.

--- frontend/src/__tests__/pages/Admin/Contacts.test.tsx
+++ frontend/src/__tests__/pages/Admin/Contacts.test.tsx
@@ -11,7 +11,7 @@ jest.mock("@react-keycloak/web", () => ({
 }));
 
 describe("<Contacts /> routing", () => {
-  it("renders Contacts page when '/admin/contacts/' is hit", () => {
+  it("renders Contacts page when '/admin/contacts/' is hit", async () => {
     const { container } = render(
       <QueryClientProvider client={queryClient}>
         <MemoryRouter initialEntries={["/admin/contacts/"]}>
