8be8d79ee944903a1bc50273705589c22cda00e0
mhaswell <michael.haswell@gov.bc.ca>
2022-08-19 16:06:50 -0700

    Added contact add/update capabilities and role capabilities. Added server-side input validation. Fixed various bugs with ministry field.

--- backend/src/models/contacts.js
+++ backend/src/models/contacts.js
@@ -3,15 +3,33 @@ const dbConnection = new DatabaseConnection();
 const db = dbConnection.knex;
 
 const table = `${dbConnection.dataBaseSchemas().data}.contact`;
+const ministryTable = `${dbConnection.dataBaseSchemas().data}.ministry`;
 
 // Get all.
 const findAll = () => {
-  return db(table).select("id", "last_name", "first_name", "contact_title", "ministry_id", "notes");
+  return db(`${table} as c`)
+    .select(
+      "c.id",
+      "c.last_name",
+      "c.first_name",
+      "c.contact_title",
+      "m.ministry_short_name",
+      "c.notes"
+    )
+    .leftJoin(`${ministryTable} as m`, "c.ministry_id", "m.id");
 };
 
 // Get specific one by id.
 const findById = (contactId) => {
-  return db(table).where("id", contactId);
+  return db(`${table} as c`)
+    .select(
+      "c.*",
+      db.raw(
+        "( SELECT json_build_object('value', m.id, 'label', m.ministry_name || ' ' || m.ministry_short_name) as ministry_id )"
+      )
+    )
+    .leftJoin(`${ministryTable} as m`, "c.ministry_id", "m.id")
+    .where("c.id", contactId);
 };
 
 // Update one.
