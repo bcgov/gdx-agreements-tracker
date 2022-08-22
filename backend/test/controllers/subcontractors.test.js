236fae76e2e2d22e6d5377d4472f45a18be05ad1
kshapka-bcgov <102179447+kshapka-bcgov@users.noreply.github.com>
2022-08-22 15:42:23 -0600

    add controller test for findById

--- backend/test/controllers/subcontractors.test.js
+++ backend/test/controllers/subcontractors.test.js
@@ -30,6 +30,7 @@ jest.mock("../../src/models/subcontractors");
 describe("Testing user controllers", () => {
   it("Gets an array of all subcontractors", async () => {
     subcontractorsModel.findAll.mockResolvedValue(subcontractors);
+
     const sampleRequest = {
       user: {
         capabilities: ["subcontractors_read_all"],
@@ -40,25 +41,28 @@ describe("Testing user controllers", () => {
     result.forEach((subcontractorsObject) => expect("id" in subcontractorsObject).toBe(true));
   });
 
-  it("Gets a single subcontractor", async () => {
-    subcontractorsModel.findById.mockResolvedValue({
-      id: 4,
-      name: "Jefferson",
-    });
+  it("Gets a single subcontractor if user has permissions", async () => {
+    subcontractorsModel.findById.mockResolvedValue([
+      {
+        id: 4,
+        subcontractor_name: "Jefferson",
+      },
+    ]);
 
-    const subcontractor = {
-      id: 4,
-      name: "Jefferson",
-    }
     const sampleRequest = {
-      params: {id: 1},
+      params: {
+        id: 4,
+      },
       user: {
-      capabilities: ["subcontractors_read_all"],
-    },};
-    
-    const result = await subcontractorsModel.findById(4).getAll(sampleRequest)
-    expect(result).toEqual(subcontractor)
+        capabilities: ["subcontractors_read_all"],
+      },
+    };
+
+    const result = await getOne(sampleRequest);
+    expect(result).toStrictEqual({ id: 4, subcontractor_name: "Jefferson" });
   });
+
+  
 });
 
 exports.subcontractors = subcontractors;
