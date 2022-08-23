ea5af8d5a41a8eb913b688568763ae8f68672d68
Shawn Turple <shawn.turple@gov.bc.ca>
2022-08-23 09:14:42 -0700

    DESCW-555 Users frontend modal

---
+++ frontend/src/pages/Admin/Users/fields.tsx
@@ -0,0 +1,40 @@
+import { FormikValues } from "formik";
+import { UseQueryResult } from "react-query";
+import { IEditFields } from "types";
+
+/**
+ * The view fields.
+ *
+ * @param   {UseQueryResult<FormikValues>} userQuery The react query data for specific resource.
+ * @returns {Array}
+ */
+export const readFields = (userQuery: UseQueryResult<FormikValues>) => {
+  return [
+    { width: "half", title: "email", value: userQuery?.data?.supplier_id.label },
+    { width: "half", title: "name", value: userQuery?.data?.subcontractor_id.label },
+  ];
+};
+
+
+/**
+ * The edit fields.
+ *
+ * @returns {Array}
+ */
+export const editFields: () => IEditFields[] = () => {
+  return [
+    {
+      fieldName: "email",
+      fieldType: "singleText",
+      fieldLabel: "Email",
+      width: "half",
+
+    },
+    {
+      fieldName: "name",
+      fieldType: "singleText",
+      fieldLabel: "Name",
+      width: "half",
+    },
+  ];
+};
