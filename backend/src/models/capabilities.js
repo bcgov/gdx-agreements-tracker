447dacde03e74421539fb5233469bf44c34a010b
Shawn Turple <shawn.turple@gov.bc.ca>
2022-08-25 09:03:18 -0700

    DESCW-556, updated changelog, and fix for null role_id

--- backend/src/models/capabilities.js
+++ backend/src/models/capabilities.js
@@ -13,11 +13,15 @@ const findAll = () => {
 
 // Given a user, retrieve all of their capabilities, as determined by their applied roles.
 const findAllByUserId = (userId) => {
-  return db(userTable)
-    .pluck("public.capabilities.name")
-    .join(rcTable, { "role_capabilities.role_id": `${userTable}.role_id` })
-    .join(cTable, { "capabilities.id": `${rcTable}.capability_id` })
-    .where("users.id", userId);
+  return (
+    db(userTable)
+      .select("public.capabilities.name")
+      //For admin users that have not set a previous value, will not have access to picklist,
+      //This is solved by coalesce, to give default users as Subscriber
+      .joinRaw(`join ${rcTable} on role_capabilities.role_id = COALESCE(${userTable}.role_id,1)`)
+      .join(cTable, { "capabilities.id": `${rcTable}.capability_id` })
+      .where("users.id", userId)
+  );
 };
 
 module.exports = {
