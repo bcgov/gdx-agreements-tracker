b96476b440b59f0d00d60360113892fd135d5988
mhaswell <michael.haswell@gov.bc.ca>
2022-08-30 17:00:42 -0700

    Added Projects route and controller for close out notify function. Updated Project CloseOut Notify component to make above API call. Updated Project CloseOut page to show Notify component only for users without edit permissions.

--- frontend/src/pages/Projects/Project/CloseOut/Notify/index.tsx
+++ frontend/src/pages/Projects/Project/CloseOut/Notify/index.tsx
@@ -1,17 +1,38 @@
 import { Box, Button, Checkbox, FormControlLabel } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { apiAxios } from "utils";
 
-// TODO: Use projectId for API call.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const Notify = ({ projectId }: { projectId: string | undefined }) => {
+  const [checked, setChecked] = useState(false);
+
+  /**
+   * Sends notify request to API.
+   *
+   * @returns {void}
+   */
+  const notify = () => {
+    if (!checked) {
+      return;
+    }
+    apiAxios()
+      .post(`projects/${projectId}/close-out/notify`)
+      .then((ret) => {
+        // eslint-disable-next-line no-console
+        console.log(ret);
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
+  };
+
   return (
     <Box>
       <FormControlLabel
-        control={<Checkbox size="small" />}
+        control={<Checkbox size="small" onChange={(e, c) => setChecked(c)} />}
         label="Check the box and click notify when ready to send a notification to the PMO staff"
       />
       <div>
-        <Button variant="contained" size="small">
+        <Button variant="contained" size="small" onClick={notify} disabled={!checked}>
           Notify
         </Button>
       </div>
