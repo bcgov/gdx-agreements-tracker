5f28ea9728a636548a43db798eb9af01063bb77b
mhaswell <michael.haswell@gov.bc.ca>
2022-08-30 17:00:42 -0700

    Added Projects route and controller for close out notify function. Updated Project CloseOut Notify component to make above API call. Updated Project CloseOut page to show Notify component only for users without edit permissions.

--- backend/src/controllers/projects.js
+++ backend/src/controllers/projects.js
@@ -68,6 +68,36 @@ const getCloseOut = async (request, reply) => {
 };
 
 /**
+ * Sends notification email when a project is closed out.
+ *
+ * @todo This should send email via CHES API. Also needs email content.
+ * @see https://bcgov.github.io/common-service-showcase/services/ches.html
+ * @param   {FastifyRequest} request FastifyRequest is an instance of the standard http or http2 request objects.
+ * @param   {FastifyReply}   reply   FastifyReply is an instance of the standard http or http2 reply types.
+ * @returns {object}
+ */
+const notifyCloseOut = async (request, reply) => {
+  userRequires(request, what, "projects_read_all");
+  let output;
+  // const targetId = Number(request.params.projectId);
+  try {
+    const message = {
+      body: "[User/contact X closed out project Y]",
+      from: "?",
+      subject: "?",
+      // Should be replaced in dev environment.
+      to: "gax.pmo@gov.bc.ca",
+    };
+    // const result = ches.send(message);
+    const result = message;
+    output = !result ? noQuery(reply, `Notification could not be sent.`) : result;
+  } catch (err) {
+    output = failedQuery(reply, err, what);
+  }
+  return output;
+};
+
+/**
  * Update an item by ID. Use passed info from the request body.
  *
  * @param   {FastifyRequest} request FastifyRequest is an instance of the standard http or http2 request objects.
@@ -90,5 +120,6 @@ module.exports = {
   getAll,
   getOne,
   getCloseOut,
+  notifyCloseOut,
   updateOne,
 };
