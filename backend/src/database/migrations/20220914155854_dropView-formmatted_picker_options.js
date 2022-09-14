95c8de6d008a5380df4adaac7d217e2f4ca7df72
Shawn Turple <shawn.turple@gov.bc.ca>
2022-09-14 12:46:20 -0700

    DESCW-574 DESCW-607 update changelog and added route test.

--- backend/src/database/migrations/20220914155854_dropView-formmatted_picker_options.js
+++ backend/src/database/migrations/20220914155854_dropView-formmatted_picker_options.js
@@ -3,5 +3,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.raw(`DROP VIEW formatted_picker_options;`);
+  return knex.raw(`DROP VIEW IF EXISTS public.formatted_picker_options;`);
 };
