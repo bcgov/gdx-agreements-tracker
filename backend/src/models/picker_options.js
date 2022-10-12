dc56d71e388b0b2079bb79d404ae0a831ca8eb73
Shawn Turple <shawn.turple@gov.bc.ca>
2022-10-12 14:38:19 -0700

    DESCW-621 Update picker options to use unions

--- backend/src/models/picker_options.js
+++ backend/src/models/picker_options.js
@@ -6,7 +6,7 @@ const pickerOptions = `${dataBaseSchemas().public}.picker_options`;
 const findAll = () => {
   return knex(pickerOptions)
     .select("name", "title", "description", knex.raw(getCaseStatements()), "associated_form")
-    .unionAll(tableLookups);
+    .unionAll(getTableLookups());
 };
 
 // Get all by project id.
@@ -20,7 +20,7 @@ const findAllByProject = (id) => {
       knex.raw(getCaseStatements(id)),
       "associated_form"
     )
-    .unionAll(tableLookups);
+    .unionAll(getTableLookups());
 };
 
 const getCaseStatements = (id) => {
@@ -65,26 +65,166 @@ const getClientCodingTableLookup = (id) => {
   return `WHEN definition ->> 'tableLookup' = 'client_coding' THEN (SELECT COALESCE(json_agg(cc), '[]') FROM (${query}) cc)`;
 };
 
-const getMinisitryPicker = () => {
+const getTablePickerQuery = (option) => {
   return knex().select(
-    knex.raw("'ministry_option' as name"),
-    knex.raw("'Client Ministry Name' as title"),
-    knex.raw("'Client Ministry Name' as description"),
+    knex.raw(`'${option.name}' as name`),
+    knex.raw(`'${option.title}' as title`),
+    knex.raw(`'${option.description}' as description`),
     knex.raw(`
     (
-      SELECT json_agg(ministry) 
+      SELECT json_agg(${option.id}) 
       FROM (
-        SELECT  id AS value, 
-        concat(ministry.ministry_name, ' ', ministry.ministry_short_name) AS label 
-        FROM data.ministry
+        SELECT  ${option.value} AS value, 
+        ${option.label} AS label 
+        FROM ${option.table}
+        ${option?.queryAdditions}
       ) 
-      ministry
+      ${option.id}
     ) as definition`),
     knex.raw("'_options' as associated_form")
   );
 };
 
-const tableLookups = [getMinisitryPicker()];
+const tableLookupValues = [
+  {
+    id: "ministry",
+    name: "ministry_option",
+    title: "Client Ministry Name",
+    description: "",
+    table: "data.ministry",
+    value: "id",
+    label: `concat(ministry.ministry_name, ' ', ministry.ministry_short_name)`,
+    queryAdditions: ``,
+  },
+  {
+    id: "fy",
+    name: "fiscal_year_option",
+    title: "Fiscal Year",
+    description: "",
+    table: "data.fiscal_year",
+    value: "id",
+    label: `fiscal_year`,
+    queryAdditions: `WHERE fiscal_year IS NOT NULL`,
+  },
+  {
+    id: "port",
+    name: "portfolio_option",
+    title: "Portfolio",
+    description: "",
+    table: "data.portfolio",
+    value: "id",
+    label: `concat(portfolio.portfolio_name, ' ', portfolio.portfolio_abbrev) `,
+    queryAdditions: ``,
+  },
+  {
+    id: "sub",
+    name: "subcontractor_option",
+    title: "Subcontractor",
+    description: "",
+    table: "data.subcontractor",
+    value: "id",
+    label: `subcontractor_name`,
+    queryAdditions: `WHERE subcontractor_name IS NOT NULL`,
+  },
+  {
+    id: "sup",
+    name: "supplier_option",
+    title: "Supplier",
+    description: "",
+    table: "data.supplier",
+    value: "id",
+    label: `supplier_name`,
+    queryAdditions: `WHERE supplier_name IS NOT NULL`,
+  },
+  {
+    id: "roles",
+    name: "user_roles_option",
+    title: "Roles",
+    description: "",
+    table: "public.roles",
+    value: "id",
+    label: `display_name`,
+    queryAdditions: `WHERE display_name IS NOT NULL`,
+  },
+  {
+    id: "contramend",
+    name: "amendment_type_option",
+    title: "Ammendment Type",
+    description: "",
+    table: "data.amendment_type",
+    value: "id",
+    label: `amendment_type_name`,
+    queryAdditions: `WHERE amendment_type_name IS NOT NULL`,
+  },
+  {
+    id: "con",
+    name: "contact_option",
+    title: "Contact",
+    description: "",
+    table: "data.contact",
+    value: "id",
+    label: `concat(contact.last_name, ', ', contact.first_name)`,
+    queryAdditions: `WHERE last_name IS NOT NULL`,
+  },
+  {
+    id: "proj",
+    name: "project_option",
+    title: "Projects",
+    description: "",
+    table: "data.project",
+    value: "id",
+    label: `project_number`,
+    queryAdditions: `WHERE project_number IS NOT NULL`,
+  },
+  {
+    id: "procurement",
+    name: "procurement_method_option",
+    title: "Procurement",
+    description: "",
+    table: "data.procurement_method",
+    value: "id",
+    label: `procurement_method`,
+    queryAdditions: `WHERE procurement_method IS NOT NULL`,
+  },
+  {
+    id: "lessonscategory",
+    name: "lesson_category_option",
+    title: "Lessons Category",
+    description: "",
+    table: "data.lesson_category",
+    value: "id",
+    label: `lesson_category_name`,
+    queryAdditions: `WHERE lesson_category_name IS NOT NULL`,
+  },
+  {
+    id: "resourceoption",
+    name: "resource_option",
+    title: "",
+    description: "",
+    table: "data.resource",
+    value: "resource_id",
+    label: `concat(resource_last_name, ', ', resource_first_name)`,
+    queryAdditions: `WHERE resource_last_name IS NOT NULL`,
+  },
+  {
+    id: "supplyrate",
+    name: "supplier_rate_option",
+    title: "Supplier Rate",
+    description: "",
+    table: "data.supplier_rate",
+    value: "supplier_rate.id",
+    label: `concat(resource_type.resource_type, ' ', supplier_rate.competency, ' - ', supplier_rate.rate)`,
+    queryAdditions: `JOIN data.resource_type  ON supplier_rate.resource_type_id = resource_type.id WHERE supplier_rate.rate IS NOT NULL`,
+  },
+];
+
+const getTableLookups = () => {
+  const unionQueries = [];
+  tableLookupValues.forEach((lookup) => {
+    unionQueries.push(getTablePickerQuery(lookup));
+  });
+  return unionQueries;
+};
 
 module.exports = {
   findAll,
