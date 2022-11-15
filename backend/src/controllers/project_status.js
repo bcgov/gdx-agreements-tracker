8f42814416ee81349c28da98a3b83c0b375b5894
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-11-15 11:32:46 -0800

    started with frontend and backend

--- backend/src/controllers/project_status.js
+++ backend/src/controllers/project_status.js
@@ -3,4 +3,18 @@ const model = require("../models/project_status");
 const what = { single: "project_status", plural: "project_statuses" };
 const controller = useController(model, what);
 
+controller.findAllByProject = async (request, reply) => {
+    controller.userRequires(request, what, `projects_read_all`);
+    try {
+      const targetId = Number(request.params.id);
+      const result = await model.findAllByProject(targetId);
+      return result ? result : [];
+    } catch (err) {
+      return controller.failedQuery(reply, err, what);
+    }
+  };
+
+  
+
 module.exports = controller;
+
--- backend/src/models/project_status.js
+++ backend/src/models/project_status.js
@@ -4,8 +4,26 @@ const { knex, dataBaseSchemas } = dbConnection();
 const table = `${dataBaseSchemas().data}.project_status`;
 
 // Get all.
-const findAll = () => {
-  return knex(table);
+const findAll = (projectId) => {
+  return knex
+    .columns(
+      "contract_amendment.id",
+      "contract.co_number as contract",
+      {
+        amendment_date: knex.raw(
+          "TO_CHAR(contract_amendment.amendment_date :: DATE, 'dd-MON-yyyy')"
+        ),
+      },
+      { amendment_type: "amendment_type.amendment_type_name" },
+      "contract_amendment.description"
+    )
+    .select()
+    .from(contractAmendmentTable)
+    .leftJoin(contractsTable, { "contract_amendment.contract_id": `${contractsTable}.id` })
+    .leftJoin(contractAmendmentTypeTable, {
+      "contract_amendment.amendment_number": `${contractAmendmentTypeTable}.id`,
+    })
+    .where({ contract_id: projectId });
 };
 
 // Get specific one by id.
