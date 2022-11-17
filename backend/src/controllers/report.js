c1ad6b1bec72b7e4bbc498af7db6bea8dd1fd8b1
kshapka-bcgov <102179447+kshapka-bcgov@users.noreply.github.com>
2022-11-17 08:27:11 -0800

    address PR comments

--- backend/src/controllers/report.js
+++ backend/src/controllers/report.js
@@ -74,10 +74,10 @@ controller.getProjectBudgetReportOnRequest = async (request, reply) => {
       project: await projectModel.findById(projectId),
       budget: await model.getProjectBudget(projectId),
       status: await projectModel.findMostRecentStatusById(projectId),
-      deliverable_summaries: await model.getDeliverableSummaries(projectId),
-      change_request: await model.getChangeRequests(projectId),
+      deliverableSummaries: await model.getDeliverableSummaries(projectId),
+      changeRequest: await model.getChangeRequests(projectId),
       contracts: await model.getContracts(projectId),
-      contract_summaries: await model.getContractSummary(projectId),
+      contractSummaries: await model.getContractSummary(projectId),
       reportDate: reportDate.toLocaleDateString("en-US", {
         day: "numeric",
         month: "numeric",
