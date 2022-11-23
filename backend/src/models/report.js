75572a8c087f82b9a211e69e7ee99533dae081d6
kshapka-bcgov <102179447+kshapka-bcgov@users.noreply.github.com>
2022-11-23 13:03:09 -0800

    rebase development

--- backend/src/models/report.js
+++ backend/src/models/report.js
@@ -373,70 +373,70 @@ const getJournalVoucher = (projectId, fiscal, quarter) => {
 
 // Get the quarterly fiscal summary for a specific project by id
 const getQuarterlyFiscalSummaries = (projectId) => {
-  const fiscal_summary = knex("data.fiscal_year as fy")
+  // Client specific summaries grouped by fiscal year
+  return knex("data.fiscal_year as fy")
     .select({
       fiscal_year: "fiscal_year",
-      detail_total: knex.sum("detail_amount"),
-      //q1_client_total: knex.sum("???"),
-      //q2_client_total: knex.sum("???"),
-      //q3_client_total: knex.sum("???"),
-      //q4_client_total: knex.sum("???"),
-      q1_fiscal_total: knex.sum("q1_amount"),
-      q2_fiscal_total: knex.sum("q2_amount"),
-      q3_fiscal_total: knex.sum("q3_amount"),
-      q4_fiscal_total: knex.sum("q4_amount"),
+      detail_client_total: knex.sum("detail_amount"),
+      q1_client_total: knex.sum("q1_amount"),
+      q2_client_total: knex.sum("q2_amount"),
+      q3_client_total: knex.sum("q3_amount"),
+      q4_client_total: knex.sum("q4_amount"),
+      client: "pb.client_coding_id",
     })
-    .leftJoin("data.project_deliverable as pd", { "fy.id": `pd.fiscal` })
+    .leftJoin("data.project_deliverable as pd", { "fy.id": "pd.fiscal" })
     .leftJoin("data.project_budget as pb", { "pd.id": "pb.project_deliverable_id" })
-    .where(`pd.project_id`, projectId)
-    .groupBy("fy.fiscal_year")
-    .orderBy(`fy.fiscal_year`);
-  return fiscal_summary;
+    .where("pd.project_id", projectId)
+    .groupBy("fy.fiscal_year", "pb.client_coding_id")
+    .orderBy("fy.fiscal_year", "pb.client_coding_id");
 };
 
 // Get the breakdown for deliverables for a specific project by id
 const getQuarterlyDeliverables = (projectId, fiscal_summary) => {
   let data = [];
   for (let fiscal in fiscal_summary) {
-    const details = knex(`data.project_deliverable as pd`)
-      .select({
-        fiscal_year: "fy.fiscal_year",
-        id: "pd.id",
-        deliverable_name: "deliverable_name",
-        detail_amount: "detail_amount",
-        q1_amount: "q1_amount",
-        q2_amount: "q2_amount",
-        q3_amount: "q3_amount",
-        q4_amount: "q4_amount",
-        resource_type: "resource_type",
-        porfolio_abbrev: "portfolio_abbrev",
-        responsibility: "responsibility",
-        service_line: "port.service_line",
-        stob: "pb.stob",
-        expense_authority_name: "expense_authority_name",
-      })
-      .leftJoin(`data.project_budget as pb`, { "pd.id": "pb.project_deliverable_id" })
-      .leftJoin(`data.client_coding as cc`, { "cc.id": "pb.client_coding_id" })
-      .leftJoin(`data.portfolio as port`, { "port.id": "pb.recovery_area" })
-      .leftJoin(`data.fiscal_year as fy`, { "fy.id": "pd.fiscal" })
-      .where({ "pd.project_id": projectId })
-      .andWhere({ "fy.fiscal_year": fiscal_summary[fiscal].fiscal_year })
-      .orderBy("deliverable_name")
-      // Construct the array of fiscal breakdown and summaries
-      .then((details) => {
-        data.push({
-          fiscal_year: fiscal_summary[fiscal].fiscal_year,
-          q1_fiscal_total: fiscal_summary[fiscal].q1_fiscal_total,
-          q2_fiscal_total: fiscal_summary[fiscal].q2_fiscal_total,
-          q3_fiscal_total: fiscal_summary[fiscal].q3_fiscal_total,
-          q4_fiscal_total: fiscal_summary[fiscal].q4_fiscal_total,
-          detail_fiscal_total: fiscal_summary[fiscal].detail_total,
-          details: details,
-        });
-      });
+    data.push(
+      knex(`data.project_deliverable as pd`)
+        .select({
+          fiscal_year: "fy.fiscal_year",
+          id: "pd.id",
+          deliverable_name: "deliverable_name",
+          detail_amount: "detail_amount",
+          q1_amount: "q1_amount",
+          q2_amount: "q2_amount",
+          q3_amount: "q3_amount",
+          q4_amount: "q4_amount",
+          resource_type: "resource_type",
+          porfolio_abbrev: "portfolio_abbrev",
+          responsibility: "responsibility",
+          service_line: "port.service_line",
+          stob: "pb.stob",
+          expense_authority_name: "expense_authority_name",
+        })
+        .leftJoin(`data.project_budget as pb`, { "pd.id": "pb.project_deliverable_id" })
+        .leftJoin(`data.client_coding as cc`, { "cc.id": "pb.client_coding_id" })
+        .leftJoin(`data.portfolio as port`, { "port.id": "pb.recovery_area" })
+        .leftJoin(`data.fiscal_year as fy`, { "fy.id": "pd.fiscal" })
+        .where({ "pd.project_id": projectId })
+        .andWhere({ "fy.fiscal_year": fiscal_summary[fiscal].fiscal_year })
+        // For client specific breakdown
+        .andWhere({ "cc.id": fiscal_summary[fiscal].client })
+        .orderBy("deliverable_name")
+        // Construct the array of fiscal breakdown and summaries
+        .then((results) => {
+          return {
+            fiscal_year: fiscal_summary[fiscal].fiscal_year,
+            q1_client_total: fiscal_summary[fiscal].q1_client_total,
+            q2_client_total: fiscal_summary[fiscal].q2_client_total,
+            q3_client_total: fiscal_summary[fiscal].q3_client_total,
+            q4_client_total: fiscal_summary[fiscal].q4_client_total,
+            detail_client_total: fiscal_summary[fiscal].detail_client_total,
+            details: results,
+          };
+        })
+    );
   }
-
-  return data;
+  return Promise.all(data);
 };
 
 /* 
