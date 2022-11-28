fae20bfa501a1d354da31d461f77377c991c84e1
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-11-28 07:36:40 -0800

    Update backend/src/controllers/projects/lessonsLearned.js
    Co-authored-by: mhaswell-bcgov <111079436+mhaswell-bcgov@users.noreply.github.com>

--- backend/src/controllers/projects/lessonsLearned.js
+++ backend/src/controllers/projects/lessonsLearned.js
@@ -1,6 +1,6 @@
 const useController = require("@controllers/useController");
 const model = require("@models/projects/lessonsLearned");
-const what = { single: "lesson learned", plural: "lesson learned" };
+const what = { single: "lesson learned", plural: "lessons learned" };
 const controller = useController(model, what, "projects");
 
 module.exports = controller;
