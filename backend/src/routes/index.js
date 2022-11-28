1d790aa8f3670066a1314cc48d4c47a44726a292
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2022-11-28 07:59:44 -0800

    updated based on PR comments

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,8 @@
 Reference :https://gist.github.com/richmarr/1122217/578a2767ec126b9b9526b6ac6df7d9935ee190d8
 */
 
+//* This function recursively goes through the routes directories and add each route file to fastify.  This is necessary because there are multiple layers of route folders.
+
 const { readdirSync } = require("fs");
 
 const getFileList = (dirName) => {
