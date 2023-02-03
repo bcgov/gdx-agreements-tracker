0d4264485854aa5fff28aa579a3c727ca268c2d0
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-02-03 09:11:31 -0800

    added helper functions for reports

---
+++ backend/src/controllers/reports/helpers/groupByProperty.js
@@ -0,0 +1,26 @@
+/**
+ * Separates an array of projects into groups by a property.
+ *
+ * @param   {any[]}   rows     Array of projects ordered by the property to be grouped on.
+ * @param   {string}  property Object property to group by.
+ * @returns {any[][]}
+ */
+const groupByProperty = (rows, property) => {
+  const groupedRows = [];
+  let currentValue = rows[0][property];
+  let currentGroup = [];
+  for (let i = 0; i < rows.length; i++) {
+    if (currentValue !== rows[i][property]) {
+      groupedRows.push(currentGroup);
+      currentValue = rows[i][property];
+      currentGroup = [];
+    }
+    currentGroup.push(rows[i]);
+  }
+  if (currentGroup.length > 0) {
+    groupedRows.push(currentGroup);
+  }
+  return groupedRows;
+};
+
+module.exports = groupByProperty;
