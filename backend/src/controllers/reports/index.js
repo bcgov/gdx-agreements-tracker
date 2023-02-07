8c60f82fd46ba0aaec0a401b73be0a70657fe034
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-02-07 12:56:59 -0800

    cleanup unneeded files and other changes

--- backend/src/controllers/reports/index.js
+++ backend/src/controllers/reports/index.js
@@ -1,16 +1,12 @@
 const useController = require("../useController/index");
 const useCommonComponents = require("../useCommonComponents/index");
 const model = require("@models/reports/index");
-const projModel = require("@models/reports/projectRollup");
 const projectModel = require("@models/projects");
 const what = { single: "report", plural: "reports" };
 const controller = useController(model, what);
 // Template and data reading
 const fs = require("fs");
 const path = require("path");
-
-// testing portfolio rollup
-const utils = require("./helpers");
 
 const cdogs = useCommonComponents("cdogs");
 const pdfConfig = { responseType: "arraybuffer" };
@@ -186,21 +182,8 @@ controller.getProjectStatusReportOnRequest = async (request, reply) => {
   try {
     const projectId = Number(request.params.id);
     const reportDate = new Date();
-
-    // portfolios testing
-    //const portfolios = request.query.portfolio;
-    //const portfolioRollup = await projModel.getRollupByPortfolios(portfolios);
-
     // Get the data from the database.
     const result = {
-      /*
-      data: {
-        report_date: "2023-02-07T16:29:06.378Z",
-        rollup: {
-          portfolios: utils.groupByProperty(portfolioRollup, "portfolio_name"),
-        },
-      },
-      */
       project: await projectModel.findById(projectId),
       deliverables: await model.projectStatusReport(projectId),
       milestones: await model.getMilestones(projectId),
@@ -208,9 +191,8 @@ controller.getProjectStatusReportOnRequest = async (request, reply) => {
       status: await projectModel.findMostRecentStatusById(projectId),
       reportDate: reportDate,
     };
-    const body = await getDocumentApiBody(result, "P_Status_MostRecent_template.docx");
+    const body = await getDocumentApiBody(result, "P_Status_MostRecent_Template.docx");
     const pdf = await cdogs.api.post("/template/render", body, pdfConfig);
-
     // Injects the pdf data into the request object.
     request.data = pdf;
     if (!result) {
