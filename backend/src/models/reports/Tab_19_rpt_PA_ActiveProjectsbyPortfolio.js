bb8f6fef04e93b8eeb7b4c65b1f50188eff40671
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-02-14 07:38:58 -0800

    intermediate changes to prep for another rebase

--- backend/src/models/reports/Tab_19_rpt_PA_ActiveProjectsbyPortfolio.js
+++ backend/src/models/reports/Tab_19_rpt_PA_ActiveProjectsbyPortfolio.js
@@ -6,10 +6,54 @@ const { knex } = dbConnection();
  *
  * @returns {any[]}
  */
-const Tab_19_rpt_PA_ActiveProjectsbyPortfolio = () => {
-  const query = knex().raw();
+const Tab_19_rpt_PA_ActiveProjectsbyPortfolio = (portfolios) => {
+  const query = knex().raw(`
+    SELECT project.portfolio_id,
+        project.planned_budget,
+        portfolio.portfolio_name AS portfolio,
+        project.project_number,
+        c.last_name || ', ' || c.first_name project_manager,
+        project.planned_start_date,
+        project.planned_end_date,
+        ministry.ministry_short_name AS client_ministry,
+        FROM (
+            data.portfolio
+            RIGHT JOIN data.project ON portfolio.id = project.portfolio_id
+            LEFT JOIN data.contact as c ON project.project_manager = c.id
+        )
+        INNER JOIN data.ministry ON project.ministry_id = ministry.id
+    WHERE(((project.project_status) = 'Active'))
+    ORDER BY portfolio,
+        project.project_number desc;
+  `);
+  if (undefined !== portfolios) {
+    if (!(portfolios instanceof Array)) {
+      portfolios = [portfolios];
+    }
+    query.whereIn("project.portfolio_id", portfolios);
+  }
 
-  return query;
+  const planned_budget_totals = knex().raw(`
+    SELECT portfolio.id as portfolio_id,
+        SUM(project.planned_budget) as TOTAL_BUDGET
+    FROM (
+            data.project
+            LEFT JOIN data.portfolio ON portfolio.id = project.portfolio_id
+        )
+    WHERE(project.project_status = 'Active')
+    GROUP BY portfolio.id;
+  `);
+  if (undefined !== portfolios) {
+    if (!(portfolios instanceof Array)) {
+      portfolios = [portfolios];
+    }
+    planned_budget_totals.whereIn("project.portfolio_id", portfolios);
+  }
+
+  return {
+    query: query,
+    planned_budget_totals: planned_budget_totals,
+  };
 };
 
 module.exports = {
--- backend/src/models/reports/activeProjectsByPortfolioQuery.sql
+++ backend/src/models/reports/activeProjectsByPortfolioQuery.sql
@@ -1,23 +1,25 @@
-SELECT Project.PortfolioID,
-    Portfolio.PortfolioName AS Portfolio,
-    Portfolio.PortfolioAbbrev,
-    Project.Fiscal,
-    Ministry.MinistryShortName AS Ministry,
-    Project.ProjectNumber AS [#],
-    Project.ProjectName AS Name,
-    Project.ProjectManager,
-    Project.Description,
-    Project.PlannedStartDate AS [Start Date],
-    Project.PlannedEndDate AS [End Date],
-    Project.PlannedBudget,
-    Project.ProjectType,
-    Project.ProjectStatus
-FROM (
-        Portfolio
-        RIGHT JOIN Project ON Portfolio.[ID] = Project.[PortfolioID]
+SELECT project.portfolio_id,
+    project.planned_budget,
+    portfolio.portfolio_name AS portfolio,
+    project.project_number,
+    c.last_name || ', ' || c.first_name project_manager,
+    project.planned_start_date,
+    project.planned_end_date,
+    ministry.ministry_short_name AS client_ministry,
+    FROM (
+        data.portfolio
+        RIGHT JOIN data.project ON portfolio.id = project.portfolio_id
+        LEFT JOIN data.contact as c ON project.project_manager = c.id
     )
-    INNER JOIN Ministry ON Project.MinistryID = Ministry.ID
-WHERE (((Project.ProjectStatus) = "Active"))
-ORDER BY Portfolio.PortfolioName,
-    Project.Fiscal DESC,
-    Project.ProjectNumber DESC;
+    INNER JOIN data.ministry ON project.ministry_id = ministry.id
+WHERE(((project.project_status) = 'Active'))
+ORDER BY portfolio,
+    project.project_number desc;
+SELECT portfolio.id as portfolio_id,
+    SUM(project.planned_budget) as TOTAL_BUDGET
+FROM (
+        data.project
+        LEFT JOIN data.portfolio ON portfolio.id = project.portfolio_id
+    )
+WHERE(project.project_status = 'Active')
+GROUP BY portfolio.id;
