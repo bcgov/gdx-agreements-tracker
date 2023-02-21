75562be0bd26cb72ad06e02b1ed27cbe18578708
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-02-21 14:14:43 -0800

    updated secrets

--- backend/src/facilities/bcgov_cc_token.js
+++ backend/src/facilities/bcgov_cc_token.js
@@ -2,14 +2,26 @@ require("dotenv").config({ path: ".env" });
 
 const chesApi = process.env.COMMON_COMPONENT_CHES_API;
 const cdogsApi = process.env.COMMON_COMPONENT_CDOGS_API;
+
+const auth = {
+  tokenHost: process.env.COMMON_COMPONENT_TOKEN_HOST,
+  tokenPath: process.env.COMMON_COMPONENT_TOKEN_PATH,
+};
+
 const config = {
-  client: {
-    id: process.env.COMMON_COMPONENT_CLIENT_ID,
-    secret: process.env.COMMON_COMPONENT_SECRET,
+  cdogs: {
+    client: {
+      id: process.env.CDOGS_CLIENT_ID,
+      secret: process.env.CDOGS_SECRET,
+    },
+    auth: auth,
   },
-  auth: {
-    tokenHost: process.env.COMMON_COMPONENT_TOKEN_HOST,
-    tokenPath: process.env.COMMON_COMPONENT_TOKEN_PATH,
+  ches: {
+    client: {
+      id: process.env.CHES_CLIENT_ID,
+      secret: process.env.CHES_SECRET,
+    },
+    auth: auth,
   },
 };
 
