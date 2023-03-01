a7d5b2d7bc30a0aae1cb1113fa7716800259b068
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-02-28 18:27:24 -0800

    DESCW-838 remove lint errors

--- backend/src/models/reports/Tab_25_rpt_PA_LessonsLearnedbyCategory.js
+++ backend/src/models/reports/Tab_25_rpt_PA_LessonsLearnedbyCategory.js
@@ -1,5 +1,4 @@
 const dbConnection = require("@database/databaseConnection");
-const { request } = require("http");
 const { knex } = dbConnection();
 
 /**
@@ -27,8 +26,6 @@ const Tab_25_rpt_PA_LessonsLearnedbyCategory = (requestParams) => {
     INNER JOIN (data.lesson_category INNER JOIN data.project_lesson ON data.lesson_category.id = data.project_lesson.lesson_category_id) 
     ON project.id = data.project_lesson.project_id`)
   );
-
-  console.log(requestParams);
 
   if (requestParams.portfolio_id) {
     query.where({ "data.project.portfolio_id": requestParams.portfolio_id });
