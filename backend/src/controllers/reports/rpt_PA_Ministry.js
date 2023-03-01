533a67742d761c484f4f65b6ab549c62291df6b1
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-03-01 12:51:35 -0800

    DESCW-967-pa-ministry-queries
    * query for ministry project usage (organized by ministry=>portfolio)
    * initial pr establishes route, controller, and model
    * using Tab_19_rpt_PA_ActiveProjectsByPortfolio as foundation

---
+++ backend/src/controllers/reports/rpt_PA_Ministry.js
@@ -0,0 +1,74 @@
+const useCommonComponents = require("../useCommonComponents/index");
+const useController = require("../useController/index");
+const model = require("@models/reports/rpt_PA_Ministry");
+const utils = require("./helpers");
+const what = { single: "report", plural: "reports" };
+const controller = useController(model, what);
+const _ = require("lodash");
+
+// Template and data reading
+const cdogs = useCommonComponents("cdogs");
+const { getReport, getDocumentApiBody, pdfConfig, groupByProperty } = utils;
+controller.getReport = getReport;
+
+/**
+ * Get a Project rollup Report for a specific array of portfolio.
+ *
+ * @param   {FastifyRequest} request FastifyRequest is an instance of the standard http or http2 request objects.
+ * @param   {FastifyReply}   reply   FastifyReply is an instance of the standard http or http2 reply types.
+ * @returns {object}
+ */
+controller.rpt_PA_Ministry = async (request, reply) => {
+  controller.userRequires(request, what, "reports_read_all");
+  try {
+    // Get the data from the database.
+    const portfolios = request.query.portfolio;
+    const activeProjects = await model.active_projects(portfolios);
+    const plannedBudgetTotals = await model.planned_budget_totals(portfolios);
+    const reportTotal = await model.report_total(portfolios);
+
+    // Chunk model info so template engine can parse it
+    const activeProjectsGroupedByPortfolioName = groupByProperty(
+      activeProjects.rows,
+      "portfolio_name"
+    );
+    const plannedBudgetTotalsKeyedByPortfolioId = _.keyBy(
+      plannedBudgetTotals.rows,
+      "portfolio_name"
+    );
+    const activeProjectsWithBudgetTotals = _.map(
+      activeProjectsGroupedByPortfolioName,
+      (portfolio) => ({
+        ...portfolio,
+        total_budget: plannedBudgetTotalsKeyedByPortfolioId[portfolio.portfolio_name].total_budget,
+      })
+    );
+    const reportTotalRow = _.first(reportTotal.rows);
+
+    // Lay out final JSON body for api call to cdogs server
+    const result = {
+      active_projects: activeProjectsWithBudgetTotals,
+      total: reportTotalRow,
+    };
+
+    /*
+    // Inject the pdf data into the request object
+    const body = await getDocumentApiBody(result, "Tab_19_rpt_PA_ActiveProjectsbyPortfolio.docx");
+    const pdf = await cdogs.api.post("/template/render", body, pdfConfig);
+    request.data = pdf;
+    */
+
+    if (!result) {
+      reply.code(404);
+      return { message: `The ${what.single} with the specified id does not exist.` };
+    } else {
+      return result;
+    }
+  } catch (err) {
+    console.error(`ERROR: ${err}`);
+    reply.code(500);
+    return { message: `There was a problem looking up this Report.` };
+  }
+};
+
+module.exports = controller;
---
+++ backend/src/models/reports/rpt_PA_Ministry.js
@@ -0,0 +1,67 @@
+const _ = require("lodash");
+const dbConnection = require("@database/databaseConnection");
+const { knex } = dbConnection();
+
+// filters query results to work around knex.raw()'s inability to chain methods
+const portfolioFilter = (portfolios) =>
+  _.isUndefined(portfolios)
+    ? `WHERE(project.project_status IN ('Active'))`
+    : `WHERE(project.project_status IN ('Active')) AND (project.portfolio_id IN (${_.castArray(
+        portfolios
+      ).join(",")}))`;
+
+module.exports = {
+  active_projects: (portfolios) => {
+    /**
+     * Gets data for the Divisional Project Reports - Project Dashboard report.
+     *
+     * @param   {number[]} portfolios Optional list of portfolio_ids to limit report to. If empty, returns data for all portfolios.
+     * @returns {any[]}
+     */
+    const active_projects = knex.raw(`
+    SELECT project.portfolio_id,
+      portfolio.portfolio_name AS portfolio_name,
+      project.project_number,
+      project.project_name,
+      c.last_name || ', ' || c.first_name project_manager,
+      project.description,
+      project.project_type,
+      project.planned_start_date,
+      project.planned_end_date,
+      project.planned_budget,
+      ministry.ministry_short_name AS client_ministry
+    FROM (
+        data.portfolio
+        RIGHT JOIN data.project ON portfolio.id = project.portfolio_id
+        LEFT JOIN data.contact as c ON project.project_manager = c.id
+    )
+    INNER JOIN data.ministry ON project.ministry_id = ministry.id
+    ${portfolioFilter(portfolios)}
+    ORDER BY portfolio_name,
+        project.project_number desc;
+  `);
+
+    return active_projects;
+  },
+
+  planned_budget_totals: (portfolios) =>
+    knex.raw(`
+      SELECT portfolio.id,
+        (SUM(project.planned_budget)) as TOTAL_BUDGET,
+        portfolio.portfolio_name
+      FROM (
+        data.project
+        RIGHT JOIN data.portfolio ON portfolio.id = project.portfolio_id
+      )
+      WHERE(project.project_status = 'Active')
+      GROUP BY portfolio.id
+      ORDER BY portfolio.portfolio_name;
+  `),
+
+  report_total: (portfolios) =>
+    knex.raw(`
+      SELECT (SUM(project.planned_budget)) as report_total
+      FROM data.project       
+      ${portfolioFilter(portfolios)}
+  `),
+};
