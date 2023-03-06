b0a87bd85a4b35078606ad277ce6986065a94bd5
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-03-06 10:53:10 -0800

    update formatting of model for pa ministry report

--- backend/src/models/reports/rpt_PA_Ministry.js
+++ backend/src/models/reports/rpt_PA_Ministry.js
@@ -13,6 +13,7 @@ const { knex } = dbConnection();
 module.exports = (portfolios, fiscalYear) => {
   const query = knex.raw(`
     SELECT 
+
     project.project_number, 
     project.project_name, 
     portfolio.portfolio_abbrev, 
@@ -22,34 +23,57 @@ module.exports = (portfolios, fiscalYear) => {
     ministry.ministry_short_name, 
     project.planned_start_date, 
     project.planned_end_date, 
-    IIF([client_coding].[client_amount] IS NULL,
-    [project].[total_project_budget],
-    [client_coding].[client_amount]) AS total_project_budget,
+
+    CASE
+      WHEN client_coding.client_amount IS NULL THEN project.total_project_budget
+      ELSE data.client_coding.client_amount
+    END AS total_project_budget,
+
+
+
     portfolio.portfolio_name,
     project.project_manager,
+
     IIF([client_coding].[client_amount] Is NULL,
-    getProjectContacts([project].[id],"client_sponsor"),
-    [contact].[first_name] & " " & [contact].[last_name]) AS client_sponsor,
+      getProjectContacts([project].[id],"client_sponsor"),
+      [contact].[first_name] || " " || [contact].[last_name]) AS client_sponsor,
+
     fiscal_year.fiscal_year,
     project.project_type
-    FROM 
-    (client_coding RIGHT JOIN 
-      (
-        (
-          (portfolio RIGHT JOIN project ON portfolio.id = project.portfolio_id) 
-          LEFT JOIN fiscal_year ON project.fiscal = fiscal_year.id
-        ) 
-        LEFT JOIN ministry ON project.ministry_id = ministry.id
-      ) 
-      ON client_coding.project_id = project.id) 
-    LEFT JOIN contact ON client_coding.contact_id = contact.id
 
-    WHERE 
-    fiscal_year.fiscal_year = [Enter Fiscal Year yy-yy] 
-    AND project.project_type = IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="E", "External", IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="I", "Internal", IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="M", "Social Media", IIF( [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?] = "S", "Service", project.project_type))))
+    FROM   (data.client_coding
+      RIGHT JOIN (((data.portfolio
+                    RIGHT JOIN data.project
+                            ON portfolio.id = project.portfolio_id)
+                   LEFT JOIN data.fiscal_year
+                          ON project.fiscal = fiscal_year.id)
+                  LEFT JOIN data.ministry
+                         ON project.ministry_id = ministry.id)
+              ON client_coding.project_id = project.id)
+     LEFT JOIN data.contact
+            ON client_coding.contact_id = contact.id
 
-    UNION ALL 
-    SELECT historical_projects.project_number,
+
+            
+
+
+  WHERE 
+  fiscal_year.fiscal_year = [Enter Fiscal Year yy-yy] 
+  AND 
+  project.project_type = IIF(
+    [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="E", "External", 
+    IIF( [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="I", "Internal",
+        IIF( [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="M", "Social Media", 
+        IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?] = "S", "Service", 
+        project.project_type
+          )
+        )
+    )
+  )
+
+  UNION ALL 
+  SELECT 
+    historical_projects.project_number,
     historical_projects.project_name,
     portfolio.portfolio_abbrev,
     historical_projects.description, 
@@ -64,21 +88,26 @@ module.exports = (portfolios, fiscalYear) => {
     NULL AS client_sponsor,
     fiscal_year.fiscal_year,
     historical_projects.project_type
-
-    FROM fiscal_year 
-    INNER JOIN (
-      ministry INNER JOIN ( 
-        portfolio INNER JOIN historical_projects ON portfolio.id = historical_projects.portfolio_id
-      ) 
-      ON ministry.id = historical_projects.ministry_id
+  FROM fiscal_year 
+  INNER JOIN (
+    ministry INNER JOIN (
+      portfolio INNER JOIN historical_projects ON portfolio.id = historical_projects.portfolio_id
     ) 
-      ON fiscal_year.id = historical_projects.fiscal_year
-    WHERE 
+    ON ministry.id = historical_projects.ministry_id
+  ) 
+    ON fiscal_year.id = historical_projects.fiscal_year
+  WHERE 
     fiscal_year.fiscal_year = [Enter Fiscal Year yy-yy] 
-    AND 
+  AND 
     historical_projects.project_type = IIF(
-      [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="E", "External", IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="I", "Internal", IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="M", "Social Media", IIF([(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?] = "S", "Service", historical_projects.project_type)))
-    );
+      [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="E", "External", IIF(
+        [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="I", "Internal", IIF(
+          [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?]="M", "Social Media", IIF(
+            [(E)xternal, (I)nternal, Social (M)edia, (S)ervice, or (A)ll?] = "S", "Service", historical_projects.project_type
+            )
+          )
+        )
+  );
   `);
 
   // filter by the portfolio list passed in from the frontend(if valid)
