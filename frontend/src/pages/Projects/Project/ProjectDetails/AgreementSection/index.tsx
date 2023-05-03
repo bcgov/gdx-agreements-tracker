0f31135f772a14b26b5bfa3c156b13d115b21574
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-05-03 08:21:07 -0700

    added dblock to agreements section (#290)

--- frontend/src/pages/Projects/Project/ProjectDetails/AgreementSection/index.tsx
+++ frontend/src/pages/Projects/Project/ProjectDetails/AgreementSection/index.tsx
@@ -1,74 +1,108 @@
-import { Box, Button } from "@mui/material";
-import { EditForm } from "components/EditForm";
-import { ReadForm } from "components/ReadForm";
-import { Renderer } from "components/Renderer";
-import { useFormSubmit } from "hooks/useFormSubmit";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { editFields, readFields } from "./fields";
+// import { Box, Button } from "@mui/material";
+// import { EditForm } from "components/EditForm";
+// import { ReadForm } from "components/ReadForm";
+// import { Renderer } from "components/Renderer";
+// import { useFormSubmit } from "hooks/useFormSubmit";
+// import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { editFields, readFields } from "./formFields";
 
-export const AgreementSection = ({
-  query,
-}: {
-  /* eslint "no-warning-comments": [1, { "terms": ["todo", "fixme"] }] */
-  // todo Define a good type. "Any" type temporarily permitted.
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  query: any;
-}) => {
+// export const AgreementSection = ({
+//   query,
+// }: {
+//   /* eslint "no-warning-comments": [1, { "terms": ["todo", "fixme"] }] */
+//   // todo Define a good type. "Any" type temporarily permitted.
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   query: any;
+// }) => {
+//   const { projectId } = useParams();
+//   const [editMode, setEditMode] = useState(false);
+//   const { handleUpdate, Notification } = useFormSubmit();
+
+//   useEffect(() => {
+//     // Certain properties when lacking a value have null labels causing errors.
+//     if (query?.data?.data) {
+//       if (null === query.data.data.agreement_type.label) {
+//         query.data.data.agreement_type.label = "";
+//       }
+//     }
+//   }, [query]);
+
+//   let content = <></>;
+//   switch (editMode) {
+//     case false:
+//     default:
+//       content = (
+//         <>
+//           <ReadForm fields={readFields(query)} />
+//           <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
+//             <Button variant="contained" onClick={() => setEditMode(true)}>
+//               Change Agreement
+//             </Button>
+//           </Box>
+//         </>
+//       );
+//       break;
+//     case true:
+//       content = (
+//         <EditForm
+//           initialValues={query?.data?.data}
+//           onSubmit={async (values) => {
+//             return handleUpdate({
+//               changedValues: values,
+//               currentRowData: query?.data?.data,
+//               apiUrl: `projects/${projectId}`,
+//               handleEditMode: setEditMode,
+//               queryKeys: [`project - ${projectId}`],
+//             });
+//           }}
+//           onCancel={() => {
+//             setEditMode(false);
+//           }}
+//           editFields={editFields()}
+//         />
+//       );
+//       break;
+//   }
+
+//   return (
+//     <>
+//       <Renderer isLoading={query.isLoading} component={content} />
+//       <Notification />
+//     </>
+//   );
+// };
+
+import { FormRenderer } from "components/FormRenderer";
+import { useFormData } from "hooks/useFormData";
+import { useParams } from "react-router";
+import { formFields } from "./formFields";
+
+/**
+ * This is a TypeScript React component that renders a form for registering a project and uses hooks to
+ * fetch and update data.
+ *
+ * @returns The `AgreementSection` component is being returned.
+ */
+
+export const AgreementSection = () => {
   const { projectId } = useParams();
-  const [editMode, setEditMode] = useState(false);
-  const { handleUpdate, Notification } = useFormSubmit();
-
-  useEffect(() => {
-    // Certain properties when lacking a value have null labels causing errors.
-    if (query?.data?.data) {
-      if (null === query.data.data.agreement_type.label) {
-        query.data.data.agreement_type.label = "";
-      }
-    }
-  }, [query]);
-
-  let content = <></>;
-  switch (editMode) {
-    case false:
-    default:
-      content = (
-        <>
-          <ReadForm fields={readFields(query)} />
-          <Box m={1} display="flex" justifyContent="flex-end" alignItems="flex-end">
-            <Button variant="contained" onClick={() => setEditMode(true)}>
-              Change Agreement
-            </Button>
-          </Box>
-        </>
-      );
-      break;
-    case true:
-      content = (
-        <EditForm
-          initialValues={query?.data?.data}
-          onSubmit={async (values) => {
-            return handleUpdate({
-              changedValues: values,
-              currentRowData: query?.data?.data,
-              apiUrl: `projects/${projectId}`,
-              handleEditMode: setEditMode,
-              queryKeys: [`project - ${projectId}`],
-            });
-          }}
-          onCancel={() => {
-            setEditMode(false);
-          }}
-          editFields={editFields()}
-        />
-      );
-      break;
-  }
+  const query = useFormData({
+    url: `/projects/${projectId}`,
+    tableName: "projects",
+    lockedRow: projectId as string,
+  });
+  const { readFields, editFields } = formFields(query.data);
 
   return (
-    <>
-      <Renderer isLoading={query.isLoading} component={content} />
-      <Notification />
-    </>
+    <FormRenderer
+      queryKey={`/projects/${projectId}`}
+      readFields={readFields}
+      editFields={editFields}
+      postUrl="/projects"
+      updateUrl={`/projects/${projectId}`}
+      query={query}
+      rowsToLock={[Number(projectId)]}
+    />
   );
 };
