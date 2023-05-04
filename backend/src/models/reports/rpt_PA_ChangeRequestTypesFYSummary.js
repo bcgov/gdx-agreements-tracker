ae19db8dd583447dc73c92a1271b09d95f084aaf
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-05-04 14:27:28 -0700

    DESCW-1074 backend generates JSON in expected format
    * ready for template

--- backend/src/models/reports/rpt_PA_ChangeRequestTypesFYSummary.js
+++ backend/src/models/reports/rpt_PA_ChangeRequestTypesFYSummary.js
@@ -10,14 +10,28 @@ const { knex } = dbConnection();
 
 const handleParams = (query, requestParams) => {
   if (requestParams.fiscal) {
-    query.where("proj.fiscal", requestParams.fiscal);
+    console.log(`
+    FISCAL: ${requestParams.fiscal}`);
+    query.where("q.fiscal", requestParams.fiscal);
   }
 };
 
 module.exports = {
   changeRequestTypes: (requestParams) => {
-    const query = knex.raw(
-      `WITH qry_changerequest_count_by_initiated_by AS (
+    const query = knex
+      .select(
+        knex.raw(
+          `
+          project_number,
+          project_name,
+          fiscal_year,
+          total_crs,
+          cr_initiated_by,
+          budget_sum,
+          schedule_sum,
+          scope_sum,
+          none_sum
+          FROM (WITH qry_changerequest_count_by_initiated_by AS (
           SELECT cr.link_id AS cr_project_id,
             cr.fiscal_year AS cr_fiscal_year_id,
             coalesce(cr.initiated_by, 'None') AS qry_cr_initiated_by,
@@ -31,6 +45,7 @@ module.exports = {
         SELECT p.project_number AS project_number,
         p.project_name AS project_name,
         fy.fiscal_year AS fiscal_year,
+        fy.id as fiscal,
         cr_count.cr_count AS total_crs,
         coalesce(cr.initiated_by, 'None') AS cr_initiated_by,
         sum(
@@ -65,16 +80,12 @@ module.exports = {
           LEFT JOIN qry_changerequest_count_by_initiated_by AS cr_count ON cr_count.cr_project_id = p.id
           AND cr_count.cr_fiscal_year_id = fy.id
           AND cr_count.qry_cr_initiated_by = coalesce(cr.initiated_by, 'None')
-        GROUP BY 1,
-          2,
-          3,
-          4,
-          5
-        ORDER BY project_number,
-          fiscal_year
-      `
-    );
-
+          GROUP BY 1,2,3,4,5,6
+        ) as q`
+        )
+      )
+      .groupByRaw("1,2,3,4,5,6,7,8,9")
+      .orderByRaw("1,3");
     handleParams(query, requestParams);
 
     return query;
