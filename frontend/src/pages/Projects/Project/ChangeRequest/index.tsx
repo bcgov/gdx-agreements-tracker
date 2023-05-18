4917cd5c5482214b996b5ab915285f7a7e27029c
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-05-18 10:24:41 -0700

    removed unused code

--- frontend/src/pages/Projects/Project/ChangeRequest/index.tsx
+++ frontend/src/pages/Projects/Project/ChangeRequest/index.tsx
@@ -1,50 +1,3 @@
-// import React from "react";
-// import { useParams } from "react-router-dom";
-// import { editFields, readFields } from "./fields";
-// import { TableComplete } from "components/TableComplete";
-
-// /**
-//  * @returns the jsx for the change request section of the project form
-//  */
-
-// export const ChangeRequest = () => {
-//   const { projectId } = useParams();
-//   const roles = {
-//     get: "projects_read_all",
-//     add: "projects_add_one",
-//     update: "projects_update_one",
-//     delete: "projects_delete_one",
-//   };
-
-//   const url = {
-//     getAll: `projects/${projectId}/change_request`,
-//     getOne: `projects/${projectId}/change_request/{id}`,
-//     updateOne: `change_request/{id}`,
-//     addOne: `/change_request`,
-//     deleteOne: `change_request/{id}`,
-//   };
-
-//   const columnWidths = {
-//     summary: 3,
-//     types: 2,
-//   };
-
-//   return (
-//     <>
-//       <TableComplete
-//         itemName="Change Request"
-//         tableName="lessons-learned"
-//         columnWidths={columnWidths}
-//         url={url}
-//         createFormInitialValues={createFormInitialValues}
-//         readFields={readFields}
-//         editFields={editFields}
-//         roles={roles}
-//       />
-//     </>
-//   );
-// };
-
 import { TableWithModal } from "components/PLAYGROUND/TableWithModal";
 import { useFormatTableData } from "components/PLAYGROUND/Table/useFormatTableData";
 import { useParams } from "react-router-dom";
@@ -53,6 +6,15 @@ import { useFormData } from "hooks/useFormData";
 import { IFormControls } from "types";
 import { tableConfig } from "./tableConfig";
 import { formConfig } from "./formConfig";
+
+/**
+ * This is a TypeScript React component that renders a table with modal for change requests related to
+ * a specific project.
+ * @returns The `ChangeRequest` component is being returned, which renders a `TableWithModal` component
+ * with `tableConfig`, `tableData`, `formControls`, `formConfig`, and `formData` as props. The
+ * `tableData` is obtained using the `useFormatTableData` hook with a specific URL path. The
+ * `formControls` is an object that contains properties and methods for handling
+ */
 
 export const ChangeRequest = () => {
   const { projectId } = useParams();
