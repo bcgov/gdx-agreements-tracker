a51058f4533146a35d04ce9148fbc6ec405c1642
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-05-25 12:48:31 -0700

    add db_lock to invoiceProcessing

---
+++ frontend/src/pages/Contracts/Contract/InvoiceProcessing/tableConfig.tsx
@@ -0,0 +1,28 @@
+import { GridColDef, GridInitialState } from "@mui/x-data-grid";
+import { useRenderTableCell } from "components/PLAYGROUND/hooks/useRenderTableCell";
+
+
+export const tableConfig = () => {
+  const defaultFlex = 3;
+  const tableColumns: GridColDef[] = [
+    { field: "received_date", headerName: "received_date", flex: defaultFlex },
+    { field: "invoice_date", headerName: "invoice_date", flex: defaultFlex },
+    { field: "due_date", headerName: "due_date", flex: defaultFlex },
+    { field: "billing_period", headerName: "billing_period", flex: defaultFlex },
+    { field: "fiscal", headerName: "fiscal", flex: defaultFlex },
+    { field: "invoice_total", headerName: "invoice_total", flex: defaultFlex },
+    { field: "invoice_number", headerName: "invoice_number", flex: defaultFlex },
+    { field: "is_gl", headerName: "is_gl", flex: defaultFlex },
+    { field: "notes", headerName: "notes", flex: defaultFlex },
+  ];
+
+  const initialState = {
+    filter: {
+      filterModel: {
+        items: [{ columnField: "received_date", operatorValue: "equals", value: "Active" }],
+      },
+    },
+  };
+
+  return { tableColumns, initialState };
+};
