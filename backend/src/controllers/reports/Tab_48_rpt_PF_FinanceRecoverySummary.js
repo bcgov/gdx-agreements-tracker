4c4f5bb1e0333ba19f8c0775d9410b691c0badf5
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-05-31 16:36:58 -0700

    clean up controller

--- backend/src/controllers/reports/Tab_48_rpt_PF_FinanceRecoverySummary.js
+++ backend/src/controllers/reports/Tab_48_rpt_PF_FinanceRecoverySummary.js
@@ -19,27 +19,22 @@ controller.getReport = getReport;
  * @returns {object}
  */
 controller.Tab_48_rpt_PF_FinanceRecoverySummary = async (request, reply) => {
-  controller.userRequires(request, "PMO-Reports-Capability", reply);
-
-  // determine whether we want to make an XLSx or PDF output
-  const {
-    query: { templateType },
-  } = request;
-
-  const templateFileName = `Tab_48_rpt_PF_FinanceRecoverySummary.${templateType}`;
-
   try {
-    const stringify = JSON.stringify;
-    // gets the data from the database
-    console.log(stringify(request?.query, null, 2));
+    controller.userRequires(request, "PMO-Reports-Capability", reply);
 
-    const getDate = async () => new Date();
+    // Determine whether we want to make an XLSx or PDF output
+    const {
+      query: { templateType },
+    } = request;
+    const templateFileName = `Tab_48_rpt_PF_FinanceRecoverySummary.${templateType}`;
+
+    // Gets the data from the database
     const [{ fiscal_year }] = await model.getFiscalYear(request.query);
     const report = await model.Tab_48_rpt_PF_FinanceRecoverySummary(request.query);
     const report_totals = await model.Tab_48_totals(request.query);
     const report_grand_totals = await model.Tab_48_grand_totals(request.query);
 
-    // shape the dataset so it can be parsed by the template rendering engine properly
+    // Shape the dataset so it can be parsed by the template rendering engine properly
     const reportByPortfolio = groupByProperty(report, "portfolio_name");
     const totalsByPortfolio = _.keyBy(report_totals, "portfolio_name");
     const reportsByPortfolioWithTotals = _.map(reportByPortfolio, (portfolio) => ({
@@ -48,18 +43,16 @@ controller.Tab_48_rpt_PF_FinanceRecoverySummary = async (request, reply) => {
     }));
 
     const result = {
-      date: await getDate(),
+      date: new Date(),
       fiscal: fiscal_year,
       report: reportsByPortfolioWithTotals,
       grand_totals: _.first(report_grand_totals),
     };
 
-    //console.log(JSON.stringify(result, null, 2));
     const body = await getDocumentApiBody(result, templateFileName, templateType);
-
     const exportFile = await cdogs.api.post("/template/render", body, pdfConfig);
 
-    // Inject the pdf data into the request object
+    // Inject the output data into the request object
     request.data = exportFile;
 
     if (!result) {
@@ -71,7 +64,9 @@ controller.Tab_48_rpt_PF_FinanceRecoverySummary = async (request, reply) => {
   } catch (err) {
     console.error(`ERROR: ${err}`);
     reply.code(500);
-    return { message: `There was a problem looking up this Project rollup Report.` };
+    return {
+      message: `There was a problem looking up this Tab_48_rpt_FinanceRecoverySummary Report.`,
+    };
   }
 };
 
