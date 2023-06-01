0fcaaf738e45c69043a4f85108a5e36e9f5b88fd
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-06-01 15:41:54 -0700

    added health colors to table

--- frontend/src/components/PLAYGROUND/hooks/useRenderTableCell/index.tsx
+++ frontend/src/components/PLAYGROUND/hooks/useRenderTableCell/index.tsx
@@ -1,13 +1,29 @@
-import { Checkbox, Link } from "@mui/material";
+import { Checkbox, Chip, Link } from "@mui/material";
 import { GridCell, GridRenderCellParams } from "@mui/x-data-grid";
+import { TableHealthChip } from "./TableHealthChip";
 
 export const useRenderTableCell = (params: GridRenderCellParams) => {
-  if (true === params.value || false === params.value) {
+  if ("boolean" === typeof params.value) {
     return (
       <GridCell {...params}>
         <Checkbox disabled checked={params.value} />
       </GridCell>
     );
   }
+
+  if (
+    params.value &&
+    params.value.red !== undefined &&
+    params.value.green !== undefined &&
+    params.value.blue !== undefined
+  ) {
+    return (
+      <GridCell {...params}>
+        {" "}
+        <TableHealthChip rgb={params.value} />
+      </GridCell>
+    );
+  }
+
   return <GridCell {...params} />;
 };
