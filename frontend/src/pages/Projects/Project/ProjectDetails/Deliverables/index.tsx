a60b24f63b5984bc9cc9b2cee4b2af18b46ed61f
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-06-14 11:54:08 -0700

    fixes for formconfig directory (#351)
    * fixes for formconfig directory
    
    * Bump app version
    
    ---------
    
    Co-authored-by: ASpiteri-BCGov <username@users.noreply.github.com>

--- frontend/src/pages/Projects/Project/ProjectDetails/Deliverables/index.tsx
+++ frontend/src/pages/Projects/Project/ProjectDetails/Deliverables/index.tsx
@@ -1,5 +1,5 @@
 import { TableWithModal } from "components/PLAYGROUND/TableWithModal";
-import { FormConfig } from "./formConfig";
+import { FormConfig } from "./FormConfig";
 import { useFormatTableData } from "components/PLAYGROUND/Table/useFormatTableData";
 import { useParams } from "react-router-dom";
 
