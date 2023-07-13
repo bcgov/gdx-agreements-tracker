db1f801de634055fb09416dac0de17965ae54d5a
Kyle Shapka <Kyle.Shapka@gov.bc.ca>
2023-07-13 12:10:06 -0700

    updated template

--- backend/src/models/reports/Tab_21_rpt_PA_Billing-Historical.js
+++ backend/src/models/reports/Tab_21_rpt_PA_Billing-Historical.js
@@ -8,109 +8,55 @@ const { knex } = dbConnection();
  * @param   {string}            portfolio - Portfolio value to filter the report.
  * @returns {Knex.QueryBuilder}        Knex query builder for fetching report totals.
  */
-const getReport = (portfolio) => {
-  const query = knex.raw(
-    `set 
-    search_path = data;
-  WITH qry_Project_Recovery_Historical as (
-    SELECT 
-      Historical_Projects.Project_Number, 
-      Historical_Projects.Project_Name, 
-      Historical_Projects.Total_Project_Budget, 
-      Fiscal_Year.Fiscal_Year AS Budget_Fiscal, 
-      Historical_Project_Billing.Q1, 
-      Historical_Project_Billing.Q2, 
-      Historical_Project_Billing.Q3, 
-      Historical_Project_Billing.Q4, 
-      cast(
-        COALESCE(Q1, '0') + COALESCE(Q2, '0') + COALESCE(Q3, '0') + COALESCE(Q4, '0') as money
-      ) AS Total_Recovered 
-    FROM 
-      Historical_Projects 
-      INNER JOIN (
-        Fiscal_Year 
-        INNER JOIN Historical_Project_Billing ON Fiscal_Year.ID = Historical_Project_Billing.Fiscal_Year
-      ) ON Historical_Projects.Project_Number = Historical_Project_Billing.Project_Number 
-    GROUP BY 
-      Historical_Projects.Project_Number, 
-      Historical_Projects.Project_Name, 
-      Historical_Projects.Total_Project_Budget, 
-      Fiscal_Year.Fiscal_Year, 
-      Historical_Project_Billing.Q1, 
-      Historical_Project_Billing.Q2, 
-      Historical_Project_Billing.Q3, 
-      Historical_Project_Billing.Q4
-  ), 
-  qry_Project_Recovery as (
-    SELECT 
-      Project.ID AS Project_ID, 
-      Project.Project_Number, 
-      Project.Project_Name, 
-      Project.Total_Project_Budget, 
-      Fiscal_Year.Fiscal_Year, 
-      Sum(
-        case when Quarter = '1' then Amount else null end
-      ) AS Q1, 
-      Sum(
-        case when Quarter = '2' then Amount else null end
-      ) AS Q2, 
-      Sum(
-        case when Quarter = '3' then Amount else null end
-      ) AS Q3, 
-      Sum(
-        case when Quarter = '4' then Amount else null end
-      ) AS Q4, 
-      Sum(JV.Amount) AS Total_Recovered 
-    FROM 
-      Fiscal_Year 
-      INNER JOIN (
-        Project 
-        INNER JOIN JV ON Project.ID = JV.Project_ID
-      ) ON Fiscal_Year.ID = JV.Fiscal_Year_ID 
-    GROUP BY 
-      Project.ID, 
-      Project.Project_Number, 
-      Project.Project_Name, 
-      Project.Total_Project_Budget, 
-      Fiscal_Year.Fiscal_Year
-  ) 
-  SELECT 
-    Project_Number, 
-    Project_Name, 
-    Total_Project_Budget, 
-    Q1, 
-    Q2, 
-    Q3, 
-    Q4, 
-    cast(
-      COALESCE(Q1, '0')+ COALESCE(Q2, '0')+ COALESCE(Q3, '0')+ COALESCE(Q4, '0') as money
-    ) AS Total_Recovered 
-  FROM 
-    qry_Project_Recovery_Historical 
-  UNION ALL 
-  SELECT 
-    Project_Number, 
-    Project_Name, 
-    Total_Project_Budget, 
-    Q1, 
-    Q2, 
-    Q3, 
-    Q4, 
-    Total_Recovered 
-  FROM 
-    qry_Project_Recovery 
-  order by 
-    Project_Number asc;
-  `
-  );
-  return query;
-};
+const projectRecoveryHistorical = knex( "historical_projects" )
+  .select({
+    project_number: "historical_projects.project_number",
+    project_name: "historical_projects.project_name",
+    total_project_budget: "historical_projects.total_project_budget",
+    budget_fiscal: "fiscal_year.fiscal_year",
+    q1: "historical_project_billing.q1",
+    q2: "historical_project_billing.q2",
+    q3: "historical_project_billing.q3",
+    q4: "historical_project_billing.q4",
+    total_recovered: knex.raw("cast( COALESCE(Q1, '0') + COALESCE(Q2, '0') + COALESCE(Q3, '0') + COALESCE(Q4, '0') as money)"),
+  })
+  .innerJoin( "fiscal_year", "historical_projects.fiscal_year", "fiscal_year.id" )
+  .innerJoin( "historical_project_billing", "historical_projects.project_number", "historical_project_billing.project_number")
+  .groupBy(
+    "historical_projects.project_number",
+    "historical_projects.project_name",
+    "historical_projects.total_project_budget",
+    "fiscal_year.fiscal_year",
+    "historical_project_billing.q1",
+    "historical_project_billing.q2",
+    "historical_project_billing.q3",
+    "historical_project_billing.q4",
+  )
+
+const projectRecovery = knex("fiscal_year")
+  .select({
+    project_id: "project.id",
+    project_number:"project.project_number",
+    project_name: "project.project_name",
+    total_project_budget: "project.total_project_budget",
+    fiscal_year: "fiscal_year.fiscal_year"
+  })
+  .innerJoin("jv", "fiscal_year.id", "jv.fiscal_year_id")
+  .innerJoin("project", "jv.project_id", "project.id")
+
+const baseQuery =knex
+  .select()
+  .from(projectRecovery.as("prh"))
+  //.where("fiscal_year", fiscal)
 
 module.exports = {
   required: ["portfolio"],
-  getAll: async (query) => {
-    const { portfolio } = query;
-    const [report] = await Promise.all([getReport(portfolio)]);
-    return { report };
+  getAll: async ({ portfolio }) => {
+    const [ report, report_totals] = await Promise.all([
+      baseQuery,
+      //reportQueries.report(portfolio),
+    ]);
+
+    return { report, report_totals };
   },
 };
