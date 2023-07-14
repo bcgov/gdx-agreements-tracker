5c7b92c31ca3d44451c9497f85dcb56d42dedf12
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-07-14 08:07:58 -0700

    updated table for admin contacts

--- frontend/src/pages/Admin/Contacts/index.tsx
+++ frontend/src/pages/Admin/Contacts/index.tsx
@@ -1,56 +1,33 @@
-import React, { FC } from "react";
-import { editFields, readFields } from "./fields";
-import { TableComplete } from "components/TableComplete";
+import { TableWithModal } from "components/PLAYGROUND/TableWithModal";
+import { useParams } from "react-router-dom";
+import { useFormControls } from "hooks";
+import { IFormControls } from "types";
+import { tableConfig } from "./tableConfig";
+import { FormConfig } from "./FormConfig";
 
-export const Contacts: FC = () => {
-  const createFormInitialValues = {
-    first_name: "",
-    address: "",
-    last_name: "",
-    city: "",
-    contact_title: "",
-    province: "",
-    ministry_id: {
-      value: 0,
-      label: "",
-    },
-    country: "",
-    contact_phone: "",
-    postal: "",
-    mobile: "",
-    website: "",
-    email: "",
-    notes: "",
-  };
+/**
+ * This is a TypeScript React component that renders a table with modal for change requests related to
+ * a specific project.
+ *
+ * @returns The `ChangeRequest` component is being returned, which renders a `TableWithModal` component
+ *  with `tableConfig`, `tableData`, `formControls`, `FormConfig`, and `formData` as props. The
+ *  `tableData` is obtained using the `useFormatTableData` hook with a specific URL path. The
+ *  `formControls` is an object that contains properties and methods for handling
+ */
 
-  const roles = {
-    get: "admin_form_read_all",
-    add: "admin_form_add_one",
-    update: "admin_form_update_one",
-    delete: "admin_form_delete_one",
-  };
+export const Contacts = () => {
+  const { contactId } = useParams();
 
-  const url = {
-    getAll: `/contacts`,
-    getOne: `/contacts/{id}`,
-    updateOne: `/contacts/{id}`,
-    addOne: `/contacts`,
-  };
-  const columnWidths = {
-    job_title: 2,
-    notes: 3,
-  };
+  const formControls: IFormControls = useFormControls();
 
   return (
-    <TableComplete
-      itemName="Contacts"
-      tableName="contact"
-      columnWidths={columnWidths}
-      url={url}
-      createFormInitialValues={createFormInitialValues}
-      readFields={readFields}
-      editFields={editFields}
-      roles={roles}
+    <TableWithModal
+      tableName={"contact"}
+      tableConfig={tableConfig()}
+      formControls={formControls}
+      formConfig={FormConfig}
+      tableDataApiEndPoint={`/contacts`}
+      formDataApiEndpoint={`/contacts/${contactId}`}
     />
   );
 };
