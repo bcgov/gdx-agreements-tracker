49f6b7d074159aaaf59ae969c0c26a14c537fef3
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-07-20 13:30:05 -0700

    descw-1252 update model for totals

--- backend/src/models/reports/Tab_38_rpt_PF_JVsforFiscal-Quarter.js
+++ backend/src/models/reports/Tab_38_rpt_PF_JVsforFiscal-Quarter.js
@@ -13,56 +13,79 @@ const reportQueries = {
   fiscalYear: (PARAMETER) =>
     knex("fiscal_year").select("fiscal_year").where("fiscal_year.id", PARAMETER).first(),
 
-  report: (PARAMETER) =>
+  report: ({ fiscal, quarter }) =>
     knex
       .select()
       .fromRaw(
         `
-        (
-
-
-
-        PASTE RAW QUERY HERE
-
-
-
-
-        ) as base`
+          (
+            SELECT
+              fy.id AS fiscal,
+              quarter,
+              p.project_number,
+              p.project_name,
+              jv.jv_number,
+              to_char(jv.billed_date, 'DD-Mon-YY'),
+              jv.amount
+            FROM
+              fiscal_year fy
+              INNER JOIN (project p RIGHT JOIN jv ON p.id = jv.project_id)
+              ON fy.id = jv.fiscal_year_id
+            ORDER BY
+              p.project_number,
+              fiscal_year DESC,
+              quarter ASC
+          ) as base
+        `
       )
-      .where("fiscal", PARAMETER),
+      .where({
+        fiscal: fiscal,
+        quarter: quarter,
+      }),
 
-  totals: (PARAMETER) =>
+  totals: ({ fiscal, quarter }) =>
     knex
       .select()
       .fromRaw(
         `
-        (
-
-
-
-        PASTE RAW QUERY HERE
-
-
-
-
-        ) as base`
+          (
+            SELECT
+              fy.id AS fiscal,
+              quarter,
+              p.project_number,
+              p.project_name,
+              jv.jv_number,
+              to_char(jv.billed_date, 'DD-Mon-YY'),
+              jv.amount
+            FROM
+              fiscal_year fy
+              INNER JOIN (project p RIGHT JOIN jv ON p.id = jv.project_id)
+              ON fy.id = jv.fiscal_year_id
+            ORDER BY
+              p.project_number,
+              fiscal_year DESC,
+              quarter ASC
+          ) as base
+        `
       )
-      .where("fiscal", PARAMETER),
+      .where({
+        fiscal: fiscal,
+        quarter: quarter,
+      })
+      .sum("amount"),
 };
 
 module.exports = {
   required: ["fiscal", "quarter"],
   getAll: async ({ fiscal, quarter }) => {
     // replace fiscal  above with whatever parameter you take here
-    const [{ fiscal_year } /*report, report_totals*/] = await Promise.all([
+    const [{ fiscal_year }, report, totals] = await Promise.all([
       reportQueries.fiscalYear(fiscal),
-      /*
-      reportQueries.report(PARAMETER),
-      reportQueries.totals(PARAMETER),
-      */
+      reportQueries.report({ fiscal, quarter }),
+      reportQueries.totals({ fiscal, quarter }),
     ]);
 
-    const reportData = { fiscal_year /*report, totals*/ };
+    const reportData = { fiscal_year, report, totals };
 
     /**
      * console.warn doesn't produce a linter error
