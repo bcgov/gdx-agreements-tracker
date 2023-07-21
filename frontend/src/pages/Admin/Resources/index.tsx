5949f9e79014f49ecd25522eae556145df900ecf
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-07-21 08:16:11 -0700

    added new table to supplier section

--- frontend/src/pages/Admin/Resources/index.tsx
+++ frontend/src/pages/Admin/Resources/index.tsx
@@ -1,44 +1,39 @@
-import React, { FC, useEffect } from "react";
-import { editFields, readFields } from "./fields";
-import { TableComplete } from "components/TableComplete";
-import useTitle from "hooks/useTitle";
 
-export const Resources: FC = () => {
+import { TableWithModal } from "components/PLAYGROUND/TableWithModal";
+import { useFormControls } from "hooks";
+import { IFormControls } from "types";
+import { tableConfig } from "./tableConfig";
+import { FormConfig } from "./FormConfig";
+import useTitle from "hooks/useTitle";
+import { useEffect } from "react";
+
+/**
+ * This is a TypeScript React component that renders a table with modal for change requests related to
+ * a specific project.
+ *
+ * @returns The `Resources` component is being returned, which renders a `TableWithModal` component
+ *  with `tableConfig`, `tableData`, `formControls`, `FormConfig`, and `formData` as props. The
+ *  `tableData` is obtained using the `useFormatTableData` hook with a specific URL path. The
+ *  `formControls` is an object that contains properties and methods for handling
+ */
+
+export const Resources = () => {
+
   const { updateTitle } = useTitle();
 
   useEffect(() => {
     updateTitle("Resources");
   }, [updateTitle]);
-
-  const createFormInitialValues = {
-    resource_last_name: "",
-    resource_first_name: "",
-    resource_id: 0,
-  };
-
-  const roles = {
-    get: "admin_form_read_all",
-    add: "admin_form_add_one",
-    update: "admin_form_update_one",
-    delete: "admin_form_delete_one",
-  };
-
-  const url = {
-    getAll: `/resources`,
-    getOne: `/resources/{id}`,
-    updateOne: `/resources/{id}`,
-    addOne: `/resources`,
-  };
+  const formControls: IFormControls = useFormControls();
 
   return (
-    <TableComplete
-      itemName="Resource"
-      tableName="resources"
-      url={url}
-      createFormInitialValues={createFormInitialValues}
-      readFields={readFields}
-      editFields={editFields}
-      roles={roles}
+    <TableWithModal
+      tableName={"resource"}
+      tableConfig={tableConfig()}
+      formControls={formControls}
+      formConfig={FormConfig}
+      tableDataApiEndPoint={`/resources`}
+      formDataApiEndpoint={`/resources/${formControls.currentRowData?.id}`}
     />
   );
 };
---
+++ frontend/src/pages/Admin/Resources/tableConfig.ts
@@ -0,0 +1,18 @@
+import { GridColDef } from "@mui/x-data-grid";
+
+export const tableConfig = () => {
+  const defaultFlex = 3;
+  const tableColumns: GridColDef[] = [
+    { field: "last_name", headerName: "Last Name", flex: defaultFlex },
+    { field: "first_name", headerName: "First Name", flex: defaultFlex },
+    { field: "subcontractor", headerName: "Subcontractor", flex: defaultFlex },
+    { field: "supplier", headerName: "Supplier", flex: defaultFlex },
+  ];
+
+  const initialState = {
+    filter: {
+    },
+  };
+
+  return { tableColumns, initialState };
+};
