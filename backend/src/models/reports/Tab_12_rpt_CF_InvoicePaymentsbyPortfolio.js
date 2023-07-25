5d6398389d650ef21c940640b8a086861ccb83f8
Kyle Shapka <Kyle.Shapka@gov.bc.ca>
2023-07-25 13:53:58 -0700

    run linter

--- backend/src/models/reports/Tab_12_rpt_CF_InvoicePaymentsbyPortfolio.js
+++ backend/src/models/reports/Tab_12_rpt_CF_InvoicePaymentsbyPortfolio.js
@@ -6,7 +6,6 @@ const _ = require("lodash");
 /**
  * Retrieves the base queries for invoice payments without filtering by fiscal year.
  *
- *
  * @returns {Promise} - A promise that resolves to the query result
  */
 const baseQueries = {
@@ -108,15 +107,14 @@ const baseQueries = {
         i.invoice_number,
         c.status,
         s.supplier_name) AS q2`
-  )
-}
+  ),
+};
 
 /**
  * Retrieves the final queries for invoice payments, filtered by fiscal year
  *
- *
  * @param   {number | string | Array} fiscal- The fiscal year to grab data for
- * @returns {Promise}                            - A promise that resolves to the query result
+ * @returns {Promise}                         - A promise that resolves to the query result
  */
 const reportQueries = {
   fiscalYear: (fiscal) =>
@@ -124,98 +122,98 @@ const reportQueries = {
 
   // The columns on which to calculate totals.
   columns: {
-    total_fee_amount: 'total_fee_amount',
+    total_fee_amount: "total_fee_amount",
     total_expense_amount: "total_expense_amount",
-    total_remaining: 'total_remaining',
-    apr: 'apr',
-    may: 'may',
-    jun: 'jun',
-    jul: 'jul',
-    aug: 'aug',
-    sep: 'sep',
-    oct: 'oct',
-    nov: 'nov',
-    dec: 'dec',
-    jan: 'jan',
-    feb: 'feb',
-    mar: 'mar',
-    remaining: 'remaining',
-    descoped: 'descoped',
+    total_remaining: "total_remaining",
+    apr: "apr",
+    may: "may",
+    jun: "jun",
+    jul: "jul",
+    aug: "aug",
+    sep: "sep",
+    oct: "oct",
+    nov: "nov",
+    dec: "dec",
+    jan: "jan",
+    feb: "feb",
+    mar: "mar",
+    remaining: "remaining",
+    descoped: "descoped",
   },
 
   // The report query, which builds off of the base queries.
-  report: (fiscal) => knex(baseQueries.q1)
-    .select({
-      portfolio_name: "q1.portfolio_name",
-      co_number: "q1.co_number",
-      start_date: knex.raw(`to_char(q1.start_date, 'DD-Mon-YY')`),
-      end_date: knex.raw(`to_char(q1.end_date, 'DD-Mon-YY')`),
-      project_number: "q1.project_number",
-      status: "q1.status",
-      total_fee_amount: "q1.total_fee_amount",
-      total_expense_amount: knex.sum("amount"),
-      total_remaining: knex.raw(`total_fee_amount+total_expense_amount-sum(amount)`),
-      apr: knex.raw(`sum(CASE WHEN q2.billing_period = ('Apr') THEN amount END)`),
-      may: knex.raw(`sum(CASE WHEN q2.billing_period = ('May') THEN amount END)`),
-      jun: knex.raw(`sum(CASE WHEN q2.billing_period = ('Jun') THEN amount END)`),
-      jul: knex.raw(`sum(CASE WHEN q2.billing_period = ('Jul') THEN amount END)`),
-      aug: knex.raw(`sum(CASE WHEN q2.billing_period = ('Aug') THEN amount END)`),
-      sep: knex.raw(`sum(CASE WHEN q2.billing_period = ('Sep') THEN amount END)`),
-      oct: knex.raw(`sum(CASE WHEN q2.billing_period = ('Oct') THEN amount END)`),
-      nov: knex.raw(`sum(CASE WHEN q2.billing_period = ('Nov') THEN amount END)`),
-      dec: knex.raw(`sum(CASE WHEN q2.billing_period = ('Dec') THEN amount END)`),
-      jan: knex.raw(`sum(CASE WHEN q2.billing_period = ('Jan') THEN amount END)`),
-      feb: knex.raw(`sum(CASE WHEN q2.billing_period = ('Feb') THEN amount END)`),
-      mar: knex.raw(`sum(CASE WHEN q2.billing_period = ('Mar') THEN amount END)`),
-      remaining: knex.raw(
-        `CASE
+  report: (fiscal) =>
+    knex(baseQueries.q1)
+      .select({
+        portfolio_name: "q1.portfolio_name",
+        co_number: "q1.co_number",
+        start_date: knex.raw(`to_char(q1.start_date, 'DD-Mon-YY')`),
+        end_date: knex.raw(`to_char(q1.end_date, 'DD-Mon-YY')`),
+        project_number: "q1.project_number",
+        status: "q1.status",
+        total_fee_amount: "q1.total_fee_amount",
+        total_expense_amount: knex.sum("amount"),
+        total_remaining: knex.raw(`total_fee_amount+total_expense_amount-sum(amount)`),
+        apr: knex.raw(`sum(CASE WHEN q2.billing_period = ('Apr') THEN amount END)`),
+        may: knex.raw(`sum(CASE WHEN q2.billing_period = ('May') THEN amount END)`),
+        jun: knex.raw(`sum(CASE WHEN q2.billing_period = ('Jun') THEN amount END)`),
+        jul: knex.raw(`sum(CASE WHEN q2.billing_period = ('Jul') THEN amount END)`),
+        aug: knex.raw(`sum(CASE WHEN q2.billing_period = ('Aug') THEN amount END)`),
+        sep: knex.raw(`sum(CASE WHEN q2.billing_period = ('Sep') THEN amount END)`),
+        oct: knex.raw(`sum(CASE WHEN q2.billing_period = ('Oct') THEN amount END)`),
+        nov: knex.raw(`sum(CASE WHEN q2.billing_period = ('Nov') THEN amount END)`),
+        dec: knex.raw(`sum(CASE WHEN q2.billing_period = ('Dec') THEN amount END)`),
+        jan: knex.raw(`sum(CASE WHEN q2.billing_period = ('Jan') THEN amount END)`),
+        feb: knex.raw(`sum(CASE WHEN q2.billing_period = ('Feb') THEN amount END)`),
+        mar: knex.raw(`sum(CASE WHEN q2.billing_period = ('Mar') THEN amount END)`),
+        remaining: knex.raw(
+          `CASE
           WHEN q1.status IN ('Complete', 'Cancelled') THEN cast(0 AS money)
           ELSE total_fee_amount+total_expense_amount-sum(amount)
-        END`),
-      descoped: knex.raw(
-        `CASE
+        END`
+        ),
+        descoped: knex.raw(
+          `CASE
           WHEN q1.status IN ('Complete', 'Cancelled') THEN total_fee_amount+total_expense_amount-sum(amount)
           ELSE cast(0 AS money)
-        END`),
-    })
-    .leftJoin(baseQueries.q2, function () {
-      this
-        .on('q1.fiscal', 'q2.fiscal')
-        .andOn('q1.contract_id', 'q2.contract_id')
-    })
-    .groupBy(
-      'portfolio_name',
-      'q1.co_number',
-      'portfolio_id',
-      'project_number',
-      'portfolio_abbrev',
-      'q1.fiscal',
-      'q1.fiscal_year',
-      'start_date',
-      'end_date',
-      'q1.status',
-      'total_fee_amount',
-      'total_expense_amount'
-    )
-    .orderByRaw(
-      `portfolio_name nulls first,
+        END`
+        ),
+      })
+      .leftJoin(baseQueries.q2, function () {
+        this.on("q1.fiscal", "q2.fiscal").andOn("q1.contract_id", "q2.contract_id");
+      })
+      .groupBy(
+        "portfolio_name",
+        "q1.co_number",
+        "portfolio_id",
+        "project_number",
+        "portfolio_abbrev",
+        "q1.fiscal",
+        "q1.fiscal_year",
+        "start_date",
+        "end_date",
+        "q1.status",
+        "total_fee_amount",
+        "total_expense_amount"
+      )
+      .orderByRaw(
+        `portfolio_name nulls first,
       q1.start_date ASC`
-    )
-    .where("q1.fiscal", fiscal),
+      )
+      .where("q1.fiscal", fiscal),
 
   // Subtotals for each portfolio.
   totals: (fiscal) =>
     knex(reportQueries.report(fiscal).as("report"))
       .select({
-        portfolio_name: "portfolio_name"
+        portfolio_name: "portfolio_name",
       })
       .sum(reportQueries.columns)
       .groupBy("portfolio_name"),
 
   // Grand totals.
   grandTotals: (fiscal) =>
-    knex(reportQueries.report(fiscal).as("report"))
-      .sum(reportQueries.columns)
+    knex(reportQueries.report(fiscal).as("report")).sum(reportQueries.columns),
 };
 
 module.exports = {
@@ -226,7 +224,7 @@ module.exports = {
       reportQueries.report(fiscal),
       reportQueries.totals(fiscal),
       reportQueries.grandTotals(fiscal),
-    ])
+    ]);
 
     // Restructure data to allow for grouping by portfolios
     const reportByPortfolio = groupByProperty(report, "portfolio_name");
