fd53ee8af27dba8e40d196fd90ce2da9a8413720
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-08-08 13:36:04 -0700

    descw-1256 build report class

--- backend/src/models/reports/Tab_31_rpt_PA_ProjectswithContracts.js
+++ backend/src/models/reports/Tab_31_rpt_PA_ProjectswithContracts.js
@@ -1,16 +1,44 @@
-//const dbConnection = require("@database/databaseConnection");
-//const { knex } = dbConnection();
+// libs
+const { knex } = require("@database/databaseConnection")();
 
-const getReport = (portfolio) => {
-  const query = "removeme"; //knex().raw();
-  return query;
+const queries = {
+  fiscal: (fiscal) =>
+    knex("fiscal_year").select("fiscal_year").where("fiscal_year.id", fiscal).first(),
+};
+
+class ReportService {
+  constructor() {
+    this.required = ["fiscal"];
+  }
+  async getReport(fiscal) {
+    const { fiscal_year } = await queries.fiscal(fiscal);
+
+    return {
+      fiscal_year,
+    };
+  }
+
+  async getAll(fiscal) {
+    const { fiscal_year } = await this.getReport(fiscal);
+    return { fiscal_year };
+  }
+}
+
+const getAll = async ({ fiscal }) => {
+  const model = new ReportService();
+  const report = await model.getAll(fiscal);
+  console.warn(`
+  inside model, getting report data:
+
+
+  ${JSON.stringify(report, null, 2)},
+
+  `);
+
+  return report;
 };
 
 module.exports = {
-  required: ["portfolio"],
-  getAll: async (query) => {
-    const { portfolio } = query;
-    const [report] = await Promise.all([getReport(portfolio)]);
-    return { report };
-  },
+  required: ["fiscal"],
+  getAll: getAll,
 };
