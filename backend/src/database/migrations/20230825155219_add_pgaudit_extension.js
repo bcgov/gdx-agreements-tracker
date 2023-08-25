cb48537fffd88421616a6ef19fba4146e3620b49
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-08-25 09:13:15 -0700

    fix linter errors

--- backend/src/database/migrations/20230825155219_add_pgaudit_extension.js
+++ backend/src/database/migrations/20230825155219_add_pgaudit_extension.js
@@ -1,8 +1,7 @@
 exports.up = function (knex) {
-    return knex.schema.raw('CREATE EXTENSION IF NOT EXISTS pgaudit');
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.raw('DROP EXTENSION IF EXISTS pgaudit');
-  };
-  
+  return knex.schema.raw("CREATE EXTENSION IF NOT EXISTS pgaudit");
+};
+
+exports.down = function (knex) {
+  return knex.schema.raw("DROP EXTENSION IF EXISTS pgaudit");
+};
