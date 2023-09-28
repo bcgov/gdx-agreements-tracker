b32fd11d7ccaee30be9b41096646fa0577957b38
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-09-28 09:50:02 -0700

    Feature/documentation frontend (#467)
    * first commit
    
    * updated to vuepress2
    
    * built first image
    
    * A spiteri bc gov patch 1 (#466)
    
    * descw-1533 api node lts 18 (#2)
    
    * get lts/Hydrogen for node setup action's config
    
    * set node version to hydrogen LTS on version bump action too
    
    * update versions based on feedback
    
    * fix workflow actions for tests and bump - node version set to lts/Hydrogen
    
    * update version
    
    * consistent image names with major and minor version numbers
    
    * update version in package.json and package-lock.json
    
    * Create deploydocs.yml
    
    ---------
    
    Co-authored-by: robrien-bcgov <richard.obrien@gov.bc.ca>
    Co-authored-by: Richard O'Brien <122112933+robrien-bcgov@users.noreply.github.com>
    
    * test
    
    * delete build
    
    * first commit
    
    * updated to vuepress2
    
    * built first image
    
    * test
    
    * A spiteri bc gov patch 1 (#466)
    
    * descw-1533 api node lts 18 (#2)
    
    * get lts/Hydrogen for node setup action's config
    
    * set node version to hydrogen LTS on version bump action too
    
    * update versions based on feedback
    
    * fix workflow actions for tests and bump - node version set to lts/Hydrogen
    
    * update version
    
    * consistent image names with major and minor version numbers
    
    * update version in package.json and package-lock.json
    
    * Create deploydocs.yml
    
    ---------
    
    Co-authored-by: robrien-bcgov <richard.obrien@gov.bc.ca>
    Co-authored-by: Richard O'Brien <122112933+robrien-bcgov@users.noreply.github.com>
    
    * delete build
    
    * changelog, version bump
    
    * update deployment config for ghpages
    
    * updated as per PR comments
    
    * updated node version
    
    ---------
    
    Co-authored-by: robrien-bcgov <richard.obrien@gov.bc.ca>
    Co-authored-by: Richard O'Brien <122112933+robrien-bcgov@users.noreply.github.com>

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,38 @@
+import { defaultTheme } from "@vuepress/theme-default";
+import { sidebar } from "./configs";
+import { defineUserConfig } from "vuepress";
+import { searchPlugin } from "@vuepress/plugin-search";
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "GDX Agreements Tracker",
+  description: "Developer Documentation for GDX Agreements Tracker",
+  theme: defaultTheme({
+    logo: "/images/BCID_H_rgb_pos.png",
+    // sidebar array
+    // all pages will use the same sidebar
+    sidebar: [
+      "/guide/README.md",
+      // SidebarItem
+      {
+        text: "React Components",
+        children: [
+          "/guide/react_components/Accordion",
+          "/guide/react_components/BudgetDisplay",
+          "/guide/react_components/BudgetDisplay",
+          "/guide/react_components/ChipNav",
+          "/guide/react_components/ConfirmationDialog",
+          "/guide/react_components/DBLock",
+          "/guide/react_components/Debug",
+          "/guide/react_components/DeleteButton",
+        ],
+      },
+      // string - page file path
+    ],
+  }),
+  plugins: [
+    searchPlugin({
+      // options
+    }),
+  ],
+});
