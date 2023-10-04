1e3d46c27dee9c170b5aa811b0363649ee2095e5
robrien-bcgov <richard.obrien@gov.bc.ca>
2023-10-04 15:55:12 -0700

    return comments to closeout controller

--- backend/src/controllers/projects/closeout.js
+++ backend/src/controllers/projects/closeout.js
@@ -3,14 +3,26 @@ const model = require("@models/projects/closeout");
 const what = { single: "project", plural: "projects" };
 const controller = useController(model, what);
 
+/**
+ * Sends notification email when a project is closed out.
+ *
+ * @todo This should send email via CHES API. Also needs email content.
+ * @see https://bcgov.github.io/common-service-showcase/services/ches.html
+ * @param   {FastifyRequest} request FastifyRequest is an instance of the standard http or http2 request objects.
+ * @param   {FastifyReply}   reply   FastifyReply is an instance of the standard http or http2 reply types.
+ * @returns {object}
+ */
 controller.notify = async (request, reply) => {
+  // const targetId = Number(request.params.projectId);
   try {
     const message = {
       body: "[User/contact X closed out project Y]",
       from: "?",
       subject: "?",
+      // Should be replaced in dev environment.
       to: "gax.pmo@gov.bc.ca",
     };
+    // const result = . ches.send(message);
     const result = message;
     return result ? result : controller.noQuery(reply, `Notification could not be sent.`);
   } catch (err) {
