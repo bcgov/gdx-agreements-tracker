ac6b5efa1814750bc539686e30579912001173a2
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-11-02 10:31:03 -0700

    Feature/descw 1516 add notify functionality (#502)
    * linter fixes
    
    * linter fixes
    
    * changelog
    
    * fixed test
    
    * linter fix

--- backend/test/routes/projects/closeout.test.js
+++ backend/test/routes/projects/closeout.test.js
@@ -23,8 +23,8 @@ describe("Close out notify function", () => {
   let testRequester;
   const test = {
     request: {
-      method: "POST",
-      url: "/projects/1/close-out/notify",
+      method: "GET",
+      url: "/projects/1/close-out",
     },
     modelFunction: model.findById,
     type: routeTypes.Specific,
@@ -32,9 +32,15 @@ describe("Close out notify function", () => {
 
   beforeEach(() => {
     const app = serverConfig();
+    // Mock getBearerTokenFromRequest to return a token string
     getBearerTokenFromRequest.mockReturnValueOnce("tokenString");
+
+    // Mock verifyToken to resolve as true
     verifyToken.mockResolvedValue(true);
+
+    // Mock getRealmRoles to return an empty array
     getRealmRoles.mockReturnValue([]);
+
     testRequester = requester(app);
   });
 
@@ -49,6 +55,9 @@ describe("Close out notify function", () => {
 
   describe("Status 401: Access routes with no user (unauthorized)", () => {
     it(`${test.request.method} - ${test.request.url}`, async () => {
+      // Mock verifyToken to resolve as false (unauthorized)
+      verifyToken.mockResolvedValue(false);
+
       const response = await testRequester.run(test.modelFunction, test.request);
       expect(response.statusCode).toBe(401);
     });
