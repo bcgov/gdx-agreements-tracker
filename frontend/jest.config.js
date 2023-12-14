90df06bfce891178ee7487d94debb1bd145f0203
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-12-14 10:28:33 -0800

    added config for code coverage

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.js", // Include all JavaScript files in the src directory and its subdirectories
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 0, // Change this number to the desired code coverage percentage
+    },
+  },
+};
