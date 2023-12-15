9982cad8ed6a816292bb18d82d55af8f42b78275
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2023-12-15 10:53:07 -0800

    pr suggestion

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     "src/**/*.js", // Include all JavaScript files in the src directory and its subdirectories
+    "src/**/*.ts", // Include all TypeScript files in the src directory and its subdirectories
   ],
   coverageThreshold: {
     global: {
