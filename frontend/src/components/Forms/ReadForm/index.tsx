1057b8626ba1da188015ce432e1d2a0f9a8b3636
robrien-bcgov <richard.obrien@gov.bc.ca>
2024-01-03 13:19:07 -0800

    DESCW-1600 document ReadForm component
    https://apps.itsm.gov.bc.ca/jira/browse/DESCW-1600

--- frontend/src/components/Forms/ReadForm/index.tsx
+++ frontend/src/components/Forms/ReadForm/index.tsx
@@ -3,6 +3,25 @@ import { IReturnValue } from "types";
 import { ReadField } from "./ReadField";
 import { FormLayout } from "components/Forms/FormLayout";
 
+/**
+ * The ReadForm component renders a form layout containing ReadField components
+ * for displaying read-only information. It takes an array of fields, each describing
+ * a specific piece of information to be displayed.
+ *
+ * @param   {Object}      props        - The component properties.
+ * @param   {Array}       props.fields - An array of field objects, each specifying the width,
+ *                                     title, value, and optionally, the type of the field.
+ * @returns {JSX.Element}              - The rendered ReadForm component.
+ *
+ * @example
+ * // Example usage of ReadForm:
+ * <ReadForm
+ *   fields={[
+ *     { width: "50%", title: "Name", value: "John Doe" },
+ *     { width: "50%", title: "Age", value: 25, type: "number" },
+ *   ]}
+ * />
+ */
 export const ReadForm = ({
   fields,
 }: {
