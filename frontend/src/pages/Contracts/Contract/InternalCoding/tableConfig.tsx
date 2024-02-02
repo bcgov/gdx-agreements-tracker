2889d5cfd4e9066b5cbcd9bdc6d1bd2b5ff79f14
robrien-bcgov <richard.obrien@gov.bc.ca>
2024-02-02 15:57:40 -0800

    descw-1988 order inputs for contract -> gdx internal coding
    * https://apps.itsm.gov.bc.ca/jira/browse/DESCW-1988

--- frontend/src/pages/Contracts/Contract/InternalCoding/tableConfig.tsx
+++ frontend/src/pages/Contracts/Contract/InternalCoding/tableConfig.tsx
@@ -5,11 +5,10 @@ export const tableConfig = () => {
 
   const tableColumns: GridColDef[] = [
     { field: "portfolio", headerName: "Portfolio", flex: defaultFlex },
+    { field: "cas_project_number", headerName: "CAS Project #", flex: defaultFlex },
     { field: "responsibility", headerName: "Responsibility", flex: defaultFlex },
     { field: "service_line", headerName: "Service Line", flex: defaultFlex },
-    { field: "cas_project_number", headerName: "CAS Project #", flex: defaultFlex },
-    { field: "asset_tag", headerName: "Asset Tag", flex: defaultFlex },
-    { field: "wip_number", headerName: "WIP #", flex: defaultFlex },
+    { field: "stob", headerName: "STOB", flex: defaultFlex },
     { field: "qualified_receiver", headerName: "Qualified Receiver", flex: defaultFlex },
     { field: "recovery_info", headerName: "Recovery Info", flex: defaultFlex },
   ];
