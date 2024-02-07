7bcac47cb2445e16b734fe372801f04424ab32b5
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-02-07 13:13:41 -0800

    fix for test fail, made hook more efficent

--- backend/src/plugins/fastifyDBLogger/index.js
+++ backend/src/plugins/fastifyDBLogger/index.js
@@ -4,28 +4,37 @@ const fp = require("fastify-plugin");
 const { getUserInfo } = require("@facilities/keycloak");
 
 const fastifyDBLogger = async (fastify) => {
-  const dbLogTable = `${dataBaseSchemas().config}.db_logs`;
+  if (process.env.NODE_ENV !== "test") {
+    const dbLogTable = `${dataBaseSchemas().config}.db_logs`;
 
-  fastify.addHook("onSend", async (request, reply, payload) => {
-    const { method, body, url } = request;
+    fastify.addHook("onSend", (request, reply, payload, done) => {
+      const { method, body, url } = request;
 
-    // Skip logging for GET and db_lock calls
-    if ("GET" === method || "OPTIONS" === method || url.includes("db_lock")) return;
-    const requestUser = await getUserInfo(request);
-    try {
-      const logData = {
-        api_method: method,
-        api_date: new Date(),
-        api_user: requestUser?.name,
-        api_body: "POST" === method ? JSON.parse(payload) : body,
-        api_url: url,
-      };
+      // Skip logging for GET and db_lock calls
+      if ("GET" === method || "OPTIONS" === method || url.includes("db_lock")) return done();
+      const requestUser = getUserInfo(request);
+      try {
+        const logData = {
+          api_method: method,
+          api_date: new Date(),
+          api_user: requestUser?.name,
+          api_body: "POST" === method ? JSON.parse(payload) : body,
+          api_url: url,
+        };
 
-      await knex(dbLogTable).insert(logData);
-    } catch (error) {
-      console.error("Database logging failed...", error);
-    }
-  });
+        knex(dbLogTable)
+          .insert(logData)
+          .then(() => done())
+          .catch((error) => {
+            console.error("Database logging failed...", error);
+            return done(error);
+          });
+      } catch (error) {
+        console.error("Database logging failed...", error);
+        return done(error);
+      }
+    });
+  }
 };
 
 module.exports = fp(fastifyDBLogger, {
