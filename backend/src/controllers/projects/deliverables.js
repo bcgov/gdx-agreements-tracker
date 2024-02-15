10b8c619f5cc93c5591cc74ea826cba3cfb27d8e
robrien-bcgov <richard.obrien@gov.bc.ca>
2024-02-15 11:09:43 -0800

    descw-2012 final touches

--- backend/src/controllers/projects/deliverables.js
+++ backend/src/controllers/projects/deliverables.js
@@ -2,7 +2,6 @@ const useController = require("@controllers/useController");
 const model = require("@models/projects/deliverables");
 const what = { single: "deliverable", plural: "deliverables" };
 const controller = useController(model, what, "projects");
-// const log = require("../../facilities/logging")(module.filename);
 
 /**
  * Updates one product_deliverable record (by ID) with null dates if the start_date or completion_date are empty strings.
@@ -18,13 +17,9 @@ const controller = useController(model, what, "projects");
  * @throws {Error} If there is an error in the query.
  */
 controller.updateOneWithNullDates = async ({ body, params }, reply) => {
-  const { start_date, completion_date } = body;
   const { id: deliverableId } = params;
-  const bodyWithNullDates = {
-    ...body,
-    start_date: "" === start_date ? null : start_date,
-    completion_date: "" === completion_date ? null : completion_date,
-  };
+  const bodyWithNullDates = nullifyDatesIfEmpty(body);
+
   try {
     const result = await model.updateOne(bodyWithNullDates, deliverableId);
     return result || controller.noQuery(reply, `The ${what.single} could not be updated.`);
@@ -45,12 +40,7 @@ controller.updateOneWithNullDates = async ({ body, params }, reply) => {
  * @throws {Error} If there is an error in the query.
  */
 controller.addOneWithNullDates = async ({ body }, reply) => {
-  const { start_date, completion_date } = body;
-  const bodyWithNullDates = {
-    ...body,
-    start_date: "" === start_date ? null : start_date,
-    completion_date: "" === completion_date ? null : completion_date,
-  };
+  const bodyWithNullDates = nullifyDatesIfEmpty(body);
 
   try {
     const result = await model.addOne(bodyWithNullDates);
@@ -58,6 +48,26 @@ controller.addOneWithNullDates = async ({ body }, reply) => {
   } catch (err) {
     return controller.failedQuery(reply, err, what);
   }
+};
+
+/**
+ * This function nullifies the start_date and completion_date in the body if they are empty strings.
+ *
+ * @function nullifyDatesIfEmpty
+ * @param   {object} body                 - The request body object.
+ * @param   {string} body.start_date      - The start date of the record.
+ * @param   {string} body.completion_date - The completion date of the record.
+ * @returns {object}                      The body object with start_date and completion_date nullified if they were empty strings.
+ */
+const nullifyDatesIfEmpty = (body) => {
+  const { start_date, completion_date } = body;
+  const nullify = (str) => ("" === str ? null : str);
+
+  return {
+    ...body,
+    start_date: nullify(start_date),
+    completion_date: nullify(completion_date),
+  };
 };
 
 module.exports = controller;
