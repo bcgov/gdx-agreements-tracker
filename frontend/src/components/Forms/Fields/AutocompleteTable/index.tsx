bda41b3ab70d57fb75e6309b1130d68a28a64caa
robrien-bcgov <richard.obrien@gov.bc.ca>
2024-02-22 11:57:29 -0800

    code cleanup

--- frontend/src/components/Forms/Fields/AutocompleteTable/index.tsx
+++ frontend/src/components/Forms/Fields/AutocompleteTable/index.tsx
@@ -32,90 +32,81 @@ export const AutocompleteTable: FC<IAutocompleteTable> = ({
     borderBottom: "1px solid #D3D3D3",
   };
 
-  return (
-    <>
-      {!pickerData?.definition?.length ? (
-        <TextField
-          disabled
-          label={fieldLabel ? fieldLabel : pickerData?.title}
-          name={fieldName}
-          fullWidth
-          required={required}
-          error
-          helperText={
-            noOptionsMessage ||
-            "There are no options available, Please contact your system administrator to resolve this issue."
-          }
-        />
-      ) : (
-        <Autocomplete
-          multiple={multiple}
-          freeSolo // allows text box to contain an arbitrary value https://mui.com/material-ui/react-autocomplete/#free-solo
-          id={fieldName}
-          autoHighlight
-          options={pickerData?.definition}
-          onChange={(e, selection) => {
-            onChange(selection);
-          }}
-          value={fieldValue}
-          getOptionLabel={(option) => {
-            return Object.values(option)[0] as string;
-          }}
-          renderInput={(params) => {
-            return (
-              <TextField
-                required={required}
-                label={fieldLabel ? fieldLabel : pickerData?.title}
-                name={fieldName}
-                {...params}
-                error={Boolean(error)}
-                helperText={helperText}
-              />
-            );
-          }}
-          renderOption={(props: object, option: IOption) => (
-            <Table>
-              {0 === pickerData?.definition.indexOf(option) && (
-                <TableHead sx={{ background: "#444", display: "flex" }}>
-                  <TableRow>
-                    {autocompleteTableColumns.map((column, index) => (
-                      <TableCell key={index} sx={{ paddingLeft: 3, width: 255, color: "#fff" }}>
-                        {column.headerName}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-              )}
-              <TableBody>
-                <TableRow {...props} sx={tableCellStyles} hover>
-                  {Object.keys(option).map(
-                    (key, index) =>
-                      key !== "value" && (
-                        <TableCell
-                          key={index}
-                          sx={{ maxWidth: 250, minWidth: 250, display: "flex", border: "none" }}
-                          component="th"
-                          scope="row"
-                        >
-                          {"boolean" === typeof option[key] ? (
-                            <ReadField
-                              width={""}
-                              title={""}
-                              value={option[key]}
-                              type={"checkbox"}
-                            />
-                          ) : (
-                            option[key]
-                          )}
-                        </TableCell>
-                      )
-                  )}
-                </TableRow>
-              </TableBody>
-            </Table>
+  return !pickerData?.definition?.length ? (
+    <TextField
+      disabled
+      label={fieldLabel ? fieldLabel : pickerData?.title}
+      name={fieldName}
+      fullWidth
+      required={required}
+      error
+      helperText={
+        noOptionsMessage ||
+        "There are no options available, Please contact your system administrator to resolve this issue."
+      }
+    />
+  ) : (
+    <Autocomplete
+      multiple={multiple}
+      freeSolo
+      id={fieldName}
+      autoHighlight
+      options={pickerData?.definition}
+      onChange={(e, selection) => {
+        onChange(selection);
+      }}
+      value={fieldValue}
+      getOptionLabel={(option) => {
+        return Object.values(option)[0] as string;
+      }}
+      renderInput={(params) => {
+        return (
+          <TextField
+            required={required}
+            label={fieldLabel ? fieldLabel : pickerData?.title}
+            name={fieldName}
+            {...params}
+            error={Boolean(error)}
+            helperText={helperText}
+          />
+        );
+      }}
+      renderOption={(props: object, option: IOption) => (
+        <Table>
+          {0 === pickerData?.definition.indexOf(option) && (
+            <TableHead sx={{ background: "#444", display: "flex" }}>
+              <TableRow>
+                {autocompleteTableColumns.map((column, index) => (
+                  <TableCell key={index} sx={{ paddingLeft: 3, width: 255, color: "#fff" }}>
+                    {column.headerName}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
           )}
-        />
+          <TableBody>
+            <TableRow {...props} sx={tableCellStyles} hover>
+              {Object.keys(option).map(
+                (key, index) =>
+                  key !== "value" && (
+                    <TableCell
+                      key={index}
+                      sx={{ maxWidth: 250, minWidth: 250, display: "flex", border: "none" }}
+                      component="th"
+                      scope="row"
+                    >
+                      {"boolean" === typeof option[key] ? (
+                        <ReadField width={""} title={""} value={option[key]} type={"checkbox"} />
+                      ) : (
+                        option[key]
+                      )}
+                    </TableCell>
+                  )
+              )}
+            </TableRow>
+          </TableBody>
+        </Table>
       )}
-    </>
+    />
   );
 };
