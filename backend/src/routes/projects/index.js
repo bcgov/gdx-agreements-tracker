f2dc5c9c141ec26fc578be2434104a7cf8f2c65e
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-02-23 13:21:36 -0800

    fixed project ministry and added back validator (#657)

--- backend/src/routes/projects/index.js
+++ backend/src/routes/projects/index.js
@@ -24,7 +24,7 @@ const routes = [
   {
     method: "POST",
     url: `/${what}`,
-    // schema: validators.addOne,
+    schema: validators.addOne,
     handler: controller.addNewProject,
   },
 ];
