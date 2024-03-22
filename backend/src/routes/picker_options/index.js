aa4522413350bee9b6a197eddb37dce2cdd8e6e6
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-03-22 09:05:19 -0700

    resolved reports to be run by all users

--- backend/src/routes/picker_options/index.js
+++ backend/src/routes/picker_options/index.js
@@ -8,36 +8,24 @@ const routes = [
     url: `/${what}`,
     schema: validators.getAll,
     handler: controller.getAll,
-    config: {
-      role: "PMO-Manager-Edit-Capability",
-    },
   },
   {
     method: "GET",
     url: `/${what}/:id`,
     schema: validators.getOne,
     handler: controller.getOne,
-    config: {
-      role: "PMO-Manager-Edit-Capability",
-    },
   },
   {
     method: "GET",
     url: `/${what}/project/:id`,
     schema: validators.getAllById,
     handler: controller.findAllByProject,
-    config: {
-      role: "PMO-Manager-Edit-Capability",
-    },
   },
   {
     method: "GET",
     url: `/${what}/contract/:id`,
     schema: validators.getAllById,
     handler: controller.findAllByContract,
-    config: {
-      role: "PMO-Manager-Edit-Capability",
-    },
   },
 ];
 
