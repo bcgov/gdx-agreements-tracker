655cf4e444237af85f48d0e37f0bf558c5aed230
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2024-03-27 10:54:48 -0700

    converted contract_status_option to db option

---
+++ frontend/src/pages/Admin/PickerOptions/ContractStatusOption/FormConfig.ts
@@ -0,0 +1,31 @@
+import { AxiosResponse } from "axios";
+import { UseQueryResult } from "@tanstack/react-query";
+import { IEditField } from "types";
+
+export const FormConfig = (query: UseQueryResult<AxiosResponse, unknown>) => {
+  const readFields = !query
+    ? []
+    : [{ width: "full", title: "Label", value: query?.data?.data?.data?.label }];
+
+  const editFields: IEditField[] = [
+    {
+      fieldName: "label",
+      fieldType: "singleText",
+      fieldLabel: "Label",
+      width: "full",
+    },
+  ];
+
+  const initialValues = {
+    label: "",
+  };
+
+  const rowId = query?.data?.data?.data?.id ?? null;
+  const rowsToLock = null === rowId ? [] : [Number(rowId)];
+  const postUrl = `/contract_status_option`;
+  const updateUrl = `/contract_status_option/${rowId}`;
+
+  const formTitle = "Contract Status Option";
+
+  return { readFields, editFields, initialValues, rowsToLock, postUrl, updateUrl, formTitle };
+};
